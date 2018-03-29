464320d94bdf295c81045a74990e70e633832e37
Andor Saga <andor.saga@gmail.com>
2018-03-29 14:31:56 -0400

    \.

--- src/chapters/fragments/code/circle_with_waves.fs
+++ src/chapters/fragments/code/circle_with_waves.fs
@@ -1,56 +1,42 @@
 precision mediump float;
 uniform float u_time;
 uniform vec2 u_res;
-#define EPSILON 0.02
-
-float circle(vec2 p, float r){
-  // len must be less than radius or radius must be > length  
-  float plen = length(p);
-  float fill = step(plen, r);
-  fill *= step(r - EPSILON, plen);
-  return fill;
-}
-
 /*
   Since we're going to overlap the teeth of the gear,
   when we specify the radius of the gear, the teeth
   are not taken into account
-
-  p - current point
   r - radius of gear (not including teeth)
-  numTeeth
-  toothHeight
 */
-float gear(vec2 p, float r, float numTeeth, float teethHeight){
+float gear(vec2 p, float r, float numTeeth, float teethHeight, float d){
   float theta = atan(p.y, p.x);
-  float isInCircle = step(length(p), r);
+  vec2 dir = normalize(p);
+  vec2 teeth = dir * sin((d * u_time + theta) *  numTeeth) * teethHeight;
+  float plen = length(p + teeth);
+  float pl = length(p);
 
+  float truncedTeeth = 1.0-step(r + teethHeight/2., length(p));
+  float isInTeeth = step(plen, r);
+  float isInCircle = step(pl, r);
+
+  return (isInCircle + isInTeeth) * truncedTeeth * step(0.4, pl);
+}
+
+float gear2(vec2 p, float r, float numTeeth, float teethHeight){
+  float theta = atan(p.y, p.x);
   // we go around the circle and create a wavy pattern.
   // we can get the length of each vector and scale it by
   // sin. but we scale the normalized vector, not the 
   // the original--that would result in a wave too large
   // normalize just to get the direction.
   vec2 dir = normalize(p);
-  vec2 t = (dir * sin(theta * numTeeth)) * 0.5;
-  vec2 p2 = p + t;
-  // p2 *= teethHeight;
-  float plen = length(p2);
-
-  // float teeth = step(plen, 1.);// * truncedStar;
-  // float isInTeeth = * step(0.5, length(p));
-
-  // return the points that are less than teethHeight
-  float teeth = step(plen, 1.0);
-
-  // return test * 1.0 - step(0.6, length(p));
-  return teeth;
-  return isInCircle;
-  // return isInCircle + isInTeeth;
+  vec2 t = (dir *  min(0.0,sin(theta * numTeeth))) * teethHeight;
+  return step(length(p + t), r);
 }
 
 void main(){
   vec2 a = vec2(1.0, u_res.y/u_res.x);
   vec2 p = a * ((gl_FragCoord.xy / u_res) * 2. -1.);
-  // Does this pixel have a part of the wavy circle?
-  gl_FragColor = vec4(vec3(gear(p, 1.0, 10.0, 0.5)), 1.0);
+  float g0 = gear(vec2(0.55, 0.) + p, 0.5, 20.0, 0.2, 1.);
+  float g1 = gear(vec2(-.55, 0.) + p, 0.5, 20.0, 0.2, -1.);
+  gl_FragColor = vec4(vec3(g0 + g1), 1.0);
 }
