2d79536f06b9ac7f20af27a8c2f94aaf1b4ac047
Andor Saga <andor.saga@gmail.com>
2018-03-29 14:37:44 -0400

    \.

--- src/chapters/fragments/code/circle_with_waves.fs
+++ src/chapters/fragments/code/circle_with_waves.fs
@@ -21,6 +21,7 @@ float gear(vec2 p, float r, float numTeeth, float teethHeight, float d){
   return (isInCircle + isInTeeth) * truncedTeeth * step(0.4, pl);
 }
 
+// same as above, but fewer lines
 float gear2(vec2 p, float r, float numTeeth, float teethHeight){
   float theta = atan(p.y, p.x);
   // we go around the circle and create a wavy pattern.
@@ -28,15 +29,19 @@ float gear2(vec2 p, float r, float numTeeth, float teethHeight){
   // sin. but we scale the normalized vector, not the 
   // the original--that would result in a wave too large
   // normalize just to get the direction.
+  float pl = length(p);
+  float truncedTeeth = 1. - step(r + teethHeight/2., pl);
   vec2 dir = normalize(p);
-  vec2 t = (dir *  min(0.0,sin(theta * numTeeth))) * teethHeight;
-  return step(length(p + t), r);
+  vec2 t = dir * min(0., sin(theta * numTeeth)) * teethHeight;
+  return step(length(p + t), r) * truncedTeeth  * step(0.4, pl);//hollow
 }
 
 void main(){
   vec2 a = vec2(1.0, u_res.y/u_res.x);
   vec2 p = a * ((gl_FragCoord.xy / u_res) * 2. -1.);
-  float g0 = gear(vec2(0.55, 0.) + p, 0.5, 20.0, 0.2, 1.);
-  float g1 = gear(vec2(-.55, 0.) + p, 0.5, 20.0, 0.2, -1.);
+  
+  float g0 = gear2(vec2(0.55, 0.) + p, 0.5, 30.0, 0.2);
+  float g1 = gear2(vec2(-.55, 0.) + p, 0.5, 30.0, 0.2);
+
   gl_FragColor = vec4(vec3(g0 + g1), 1.0);
 }
