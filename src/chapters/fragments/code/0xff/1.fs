174df554d487396fb4337d36149bb6b68e66c82a
Andor Saga <andor.saga@gmail.com>
2018-04-05 15:29:43 -0400

    polishing.

--- src/chapters/fragments/code/0xff/1.fs
+++ src/chapters/fragments/code/0xff/1.fs
@@ -1,7 +1,7 @@
 precision mediump float;
 uniform vec2 u_res;
 uniform float u_time;
-
+#define PI 3.14159
 float square(vec2 p, float size){
   // Does this fragment fall inside the square?  
   // determine which side of the square this fragment is on
@@ -21,17 +21,16 @@ float rectSDF(vec2 p, vec2 size){
   return step(side, size.x) + step(side, size.y);
 }
 
+
+
 void main(){
   vec2 a = vec2(1., u_res.y/u_res.x);
   vec2 p = a * (gl_FragCoord.xy/u_res*2.-1.);
   float h = 0.08;
-
-  float t = u_time;
-  p.y += sin(p.x* 5. + t) * 0.15;
-  // float s = sin((p.x + t) * 2.) * 0.5;
-  // float waveSize = 0.2;
+  float t = u_time * 3.25;
+  p.y += sin(p.x * 7.0 + t) * 0.1;
   
-  float c = rectSDF(vec2(p.x, p.y - .25), vec2(0.8, h)) +
+  float c = rectSDF(vec2(p.x, p.y - .25), vec2(0.8, h)) + 
   			rectSDF(vec2(p.x, p.y - .0), vec2(0.8, h)) + 
   			rectSDF(vec2(p.x, p.y + .25), vec2(0.8, h));
 
