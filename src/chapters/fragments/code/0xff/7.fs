f46b974911875fa9a906caa08d60a7e283e91111
Andor Saga <andor.saga@gmail.com>
2018-04-12 14:40:47 -0400

    almost finished debugging.

--- src/chapters/fragments/code/0xff/7.fs
+++ src/chapters/fragments/code/0xff/7.fs
@@ -2,14 +2,51 @@ precision mediump float;
 uniform vec2 u_res;
 uniform float u_time;
 
+#define PI 3.1415926
+#define TAU 2.*PI
+#define O 1.
+#define Z 0.
+
 float sphereSDF(vec2 p, float r){
   return length(p) - r;
 }
 
+float circleSDF(vec2 p, float r){
+  return length(p) - r;
+}
+
 void main(){
-  vec2 a = vec2(1., u_res.y/u_res.x);
-  vec2 p = a * (gl_FragCoord.xy / u_res * 2. -1.);
+  vec2 a = vec2(1.,u_res.y/u_res.x);
+  vec2 p = a* (gl_FragCoord.xy / u_res * 2. -1.);
+  float t = u_time;
+
+  vec3 sunDir = vec3(cos(t), Z, sin(t));
+  sunDir = normalize(sunDir);
+
+  // vec3 v = 1. - vec3(length(p) - 0.25);
+  // vec3 c = vec3(step(circleSDF(p, 0.5), 0.5));
+  // vec3 v = vec3(step(circleSDF(p, 0.5), 0.5));
+
+  if(length(p) > 1.0){
+    discard;
+  }
+
+  // Need to somehow create z coordinate from 2D
+  float z = (sin(p.y * 1.)  - p.x) + (cos(p.x * 1.) - p.y);
+
+  // float z = 0.0;
+
+  // B = [1,0,0]
+  // cos(1 * PI) + sin(0 * PI) 
+  // cos(PI) + sin(0)
+  // -1 + 0
+
+  vec3 v = vec3(p.x, p.y, z);
+  v = normalize(v);
   
-  float i = 1. - step(0.5, sphereSDF(p, 0.5));
-  gl_FragColor = vec4(vec3(i), 1.);
+  vec3 i = vec3(dot(v, sunDir));
+  // c = vec3(step(c.x, 0.5));
+
+  // i = step(vec3(0.4), i);
+  gl_FragColor = vec4(i, 1.);
 }
