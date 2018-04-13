e3ee43f54d3af7646d7f234a4628689e7a0335e4
Andor Saga <andor.saga@gmail.com>
2018-04-13 12:19:30 -0400

    finished 8.

--- src/chapters/fragments/code/0xff/8.fs
+++ src/chapters/fragments/code/0xff/8.fs
@@ -1,21 +1,17 @@
 precision mediump float;
 uniform vec2 u_res;
 uniform float u_time;
-#define TAU 3.141592658 * 2.
+#define TAU 3.14159265834*2.
 
-vec3 s(vec2 p, float m){
-  vec3 test = vec3(.0);
-  if(p.x < .5 && p.y < .5) test = vec3(0.);
-  else if(p.x < .5 && p.y > .5) test = vec3(1.);
-  else if(p.x > .5 && p.y < .5) test = vec3(1.);
-  return test;
+float s(vec2 p){
+  vec2 a = vec2(step(p, vec2(.5)));
+  return a.x == 0. && a.y == 0. ? 1. : a.x * a.y;
 }
 
 void main(){
   vec2 a = vec2(1., u_res.y/u_res.x);
   vec2 p = a * (gl_FragCoord.xy/ u_res * 2. -1.);
-  float r = mod(.5 / length(p) + u_time, 1.);
+  float r = mod(.5 / length(p) + u_time*.5, 1.);
   float th = mod(atan(p.y / p.x)/TAU, 1.);
-  vec3 i = s(vec2(r, th), r);
-  gl_FragColor = vec4(i, 1.);
+  gl_FragColor = vec4(vec3(s(vec2(r, th))), 1.);
 }
