9850343554ee22e273713cfeca7d667a20a82771
Andor Saga <andor.saga@gmail.com>
2018-04-15 18:01:49 -0400

    working on triangle SDF.

--- src/chapters/fragments/code/0xff/10.fs
+++ src/chapters/fragments/code/0xff/10.fs
@@ -1,24 +1,21 @@
 precision mediump float;
 uniform vec2 u_res;
-uniform float u_time;
+#define COS_30 0.8660256249
 
 float triangleSDF(vec2 p, float s){  
-	// Y AXIS TO POINT
-  float horizDist = p.x/2. + .5;
-  float triangleHorizDist = -(p.y/2.) + .5;
-  return 0.8660256249 * (horizDist - triangleHorizDist);
-
-float v = 0.0;
-  return 0.8660256249 * (p.x - v);
+  float yDist = -p.y/2.+.5;
+  
+  float l = -p.x - yDist;
+  float r = p.x - yDist;
+  
+  return (l*COS_30) * (r*COS_30);
 }
 
 void main(){
-  // vec2 a = vec2(1., u_res.y/u_res.x);  
-  vec2 p = vec2(gl_FragCoord.xy/u_res * 2. -1.);
+  vec2 a = vec2(1., u_res.y/u_res.x);  
+  vec2 p = a * vec2(gl_FragCoord.xy/u_res * 2. -1.);
 
-  // vec2 p = a * vec2(gl_FragCoord.xy/u_res);
-
-  float i = step(triangleSDF(p, 1.), 0.1);
-  // float i = triangleSDF(vec2(-p.x, p.y), 1.);
+  
+  float i = 1. - step(triangleSDF(p, 1.), 0.0);
   gl_FragColor = vec4(vec3(i), 1.);
 }
