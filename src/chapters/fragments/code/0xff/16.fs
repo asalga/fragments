fc5ddc53f0c943de30dddd18009822d9b1791bb5
Andor Saga <andor.saga@gmail.com>
2018-04-19 15:12:56 -0400

    16

---
+++ src/chapters/fragments/code/0xff/16.fs
@@ -0,0 +1,36 @@
+precision mediump float;
+uniform vec2 u_res;
+uniform float u_time;
+#define COS_30 0.8660254034;
+#define TAU 2.*3.141592658
+
+float tri(vec2 p, float s){
+  vec2 a = abs(p);
+  float distToSide = a.x * COS_30;
+  float u = p.y * .28;
+  float _1 = distToSide + u;
+  float _2 = -u;
+  float m = max(_1,_2);
+  if(p.y < -0.1) return 1.;
+  if(p.x < 0.0 && p.y < 0.37)return 1.; 
+  return m - (s*1.2);
+}
+float tSDF(vec2 p, float s){
+  return tri(p, s) * (2.*tri(p, s*0.8));
+}
+mat2 r2d(float _a){
+  return mat2(cos(_a),-sin(_a),sin(_a),cos(_a));
+}
+void main(){
+  vec2 a = vec2(1., u_res.y/u_res.x);
+  vec2 p = a*(gl_FragCoord.xy/u_res*2.-1.);
+  p *=1.2;
+  float c = 0.;
+  float t = 0.;
+  for(int i = 0; i < 5; i++){
+  	mat2 rot = r2d(TAU/5.*float(i));
+    c += step(tSDF(p * rot, .24), 0.)
+     - 2.*step(tSDF(p * rot, .2), 0.);
+  }
+  gl_FragColor = vec4(c,c,c, 1.);
+}
