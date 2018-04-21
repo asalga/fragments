b358f3a60785fb397048ffd8ce6cdca21dd2fd13
Andor Saga <andor.saga@gmail.com>
2018-04-21 13:13:23 -0400

    \.

--- src/chapters/fragments/code/0xff/18.fs
+++ src/chapters/fragments/code/0xff/18.fs
@@ -23,38 +23,24 @@ void main(){
   float theta = acos(dot(n,ldir));
 
   // how to distinguish between vertical and horiz
-
-  // normalize theta
   float i = 1. - (theta / TAU);
   float r = 1./NUM_STRIPS;
-
-
-  // normals with the same theta will create 'circles'
-
 
   vec3 xzVec = vec3(n.x, 0., n.z);
   xzVec = normalize(xzVec);
 
-  vec3 yzVec = vec3(n.x, n.y, 0.);
+  vec3 yzVec = vec3(n.y, 0., n.z);
   yzVec = normalize(yzVec);
 
   float xzVecTheta = acos(dot(xzVec,ldir))/TAU;
   float yzVecTheta = acos(dot(yzVec,ldir))/TAU;
 
-  i = step(mod(xzVecTheta, r), r/2.);
-  
+  i = step(mod(xzVecTheta, r), r/2.) * 
+      step(mod(yzVecTheta, r), r/2.);
   
   // create a vector from (0,0,1) and rotate it
   // all the normals running down vertically a long the
   // sphere will have the same theta
-
-
-
-  // normals that create strips
-
-
   // step(cSDF(p, .5), 0.);
-
-
   gl_FragColor = vec4(vec3(i),1.);
 }
