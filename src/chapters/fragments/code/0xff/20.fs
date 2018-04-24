c9c69f1083ed2fc2aa6ca279da009b8030b91e62
Andor Saga <andor.saga@gmail.com>
2018-04-24 11:53:36 -0400

    finished 20.

--- src/chapters/fragments/code/0xff/20.fs
+++ src/chapters/fragments/code/0xff/20.fs
@@ -4,7 +4,6 @@ uniform float u_time;
 uniform vec3 u_mouse;
 uniform vec2 u_tracking;
 #define PI 3.141592658
-
 float cSDF(vec2 p, float r){
   return length(p)-r;
 }
@@ -17,37 +16,21 @@ float lens(vec2 p, float r, float n){
   return a*b;
 }
 void main(){
-  vec3 ldir = vec3(0., 0., 1.);
-  ldir = normalize(ldir);
+  vec3 ldir = normalize(vec3(0., 0.52, 1.));
   vec2 a = vec2(1., u_res.y/u_res.x);
   vec2 p = a*(gl_FragCoord.xy/u_res*2.-1.);
   vec2 m = vec2(-.4,.1) * (u_tracking*2.-1.);
-  float i = cSDF(p, 0.5);
-
-  float b = 0.1;
-  // ldir = vec3(cos((b+m.x)*.1), .0, sin((b+m.x)*.1));
-  ldir = vec3(0., 0., 1.);
-
-  float sz = 2.;
-  float x=(p.x)*sz;//+m.x
-  float y=(p.y)*sz;//+m.y
-  float z = sqrt(1.-x*x-y*y);
-
-  // do we need to normalize??
-  vec3 n = normalize(vec3(p, z));
-
-  float theta = acos(dot(ldir, n));
-  theta = 1.-((theta*2.) /PI);
-  
-  // theta *= 30.;
-  theta = step(theta, 0.1);
-  
-
-  // i=
-   i = step(1., lens(p, .5, .5))
-  - smoothstep(.01,.001,ringSDF(p + m, .7, .15))
-  + smoothstep(.01,.001,cSDF(p+vec2(.25, -.25) + m, .05))
+  vec2 pcopy = vec2(p);
+  ldir = vec3(cos(m.x), .0, sin(m.x));
+  pcopy = (pcopy+m) * 2.15;
+  float z = (sqrt(1.-pcopy.x*pcopy.x-pcopy.y*pcopy.y));
+  vec3 n = vec3(p, z);
+  float theta = acos(dot(ldir, normalize(vec3(n.x, n.y, n.z))));
+  theta = step(0.3, 1.-((theta*2.) /PI));
+  float i = step(1., lens(p, .48, .5));
+  i -= smoothstep(.01,.001,ringSDF(p + m, .7, .15));
+  i += smoothstep(.01,.001,cSDF(p+vec2(.25, -.25) + m, .05))
   + theta;
-
+  i += 2.* (step(1., lens(p, .52, .5)) - step(1., lens(p, .5, .5))); // fix
   gl_FragColor = vec4(vec3(i), 1.);
 }
