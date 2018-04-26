9fc9d7ff74389d50293679f946946295f50be72c
Andor Saga <andor.saga@gmail.com>
2018-04-25 23:39:26 -0400

    .

--- src/chapters/fragments/code/sdf/rect.fs
+++ src/chapters/fragments/code/sdf/rect.fs
@@ -3,12 +3,23 @@
 precision mediump float;
 uniform vec2 u_res;
 
-// 
 float rrectSDF(vec2 p, vec2 d, float r){
   vec2 diff = abs(p) - d;
   float test = length(p)-r;
   return min(max(diff.x, diff.y), test);
 }
+
+float roundRect(vec2 p, vec2 size, float radius) {
+  vec2 d = abs(p) - size;
+  return min(max(d.x, d.y), 0.0) + length(max(d,0.0))- radius;
+}
+
+
+float ellipse(vec2 p, vec2 r, float s) {
+    return (length(p / r) - s);
+}
+
+ // return length(vec2(max(a.x-w,0.), a.y));
 
 void main(){
   vec2 a = vec2(1., u_res.y/u_res.x);
