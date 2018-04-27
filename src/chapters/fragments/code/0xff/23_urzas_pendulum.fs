0287375cc10ce93ce9c689f5ea1e1a49d37fffc5
Andor Saga <andor.saga@gmail.com>
2018-04-27 17:59:13 -0400

    polishing 23.

--- src/chapters/fragments/code/0xff/23_urzas_pendulum.fs
+++ src/chapters/fragments/code/0xff/23_urzas_pendulum.fs
@@ -6,37 +6,51 @@ uniform vec3 u_mouse;
 uniform vec2 u_lastMouseDown;
 #define PI 3.141592658
 #define TAU PI * 2.
+
+// TODO:
+// - add light
+
+ 
+
+
+// capsule definition source from book of shaders
+float capsule(vec2 p, vec2 a, vec2 b, float r) {
+  vec2 pa = p - a, ba = b - a;
+  float h = clamp( dot(pa,ba)/dot(ba,ba), 0.0, 1.0 );
+  return length(pa-ba*h)-r;
+}
 float cSDF(vec2 p, float r){return length(p) - r;}
 void main(){
   float t = u_time * 10.;
   vec2 ar = vec2(1.,u_res.y/u_res.x);
   float friction = max(0.,1.-(u_time/2.));
-  
   vec2 p = ar*(gl_FragCoord.xy/u_res * 2. -1.);p.y-= 1.;
-  
+
   vec2 m = vec2(-1.,1.)*ar*(u_mouse.xy/u_res*2.-1.);
   vec2 lm = vec2(-1.,1.)*ar*(u_lastMouseDown/u_res*2.-1.);
   lm.y +=1.;m.y +=1.;
 
   float armLength = length(lm);
-  vec2 point = normalize(vec2(abs(lm.x), lm.y));
+  // we already havce the length, no point in calling 
+  // normlized which calcs len again.
+  vec2 point = lm/armLength;
   vec2 downVec = vec2(0.,1.);
   float d = dot(downVec, point);
-  float swingMag = acos(d);
-
-  float theta = cos(t) * swingMag * friction;
+  float swingMag = -acos(d);
+  
+  // depending on where user clicked. left or right side.
+  swingMag *= step(0.0, lm.x)*2. -1.;
+  
+  float theta =  cos(t) * swingMag * friction;
 	vec2 bPos = p + armLength * (vec2(-sin(theta), cos(theta)));
 
-	if(u_mouse.z == 1. && u_lastMouseDown.x < -10.){
+	if(u_mouse.z == 1.){
     bPos = p + m;
   }
- 
-	float i = step(cSDF(bPos, 0.25), 0.);
-  i += step(cSDF(p, 0.05), 0.);
+
+	float i = smoothstep(0.01, 0.001, cSDF(bPos, 0.25)) +
+            smoothstep(0.01, 0.001, cSDF(p, 0.05)) + 
+            smoothstep(0.01, 0.001, capsule(p, vec2(0.), p-bPos, 0.005));
 
   gl_FragColor = vec4(vec3(i), 1.);
-
-  if (abs(p.x) < 0.01 || abs(p.y) < 0.01){
-    gl_FragColor = vec4(1.);
-  }
 }
