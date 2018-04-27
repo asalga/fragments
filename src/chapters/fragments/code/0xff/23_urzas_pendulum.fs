fc5b9abba80363427fcb22cebf9b55dcfacd2b22
Andor Saga <andor.saga@gmail.com>
2018-04-27 12:00:38 -0400

    refactoring.

--- src/chapters/fragments/code/0xff/23_urzas_pendulum.fs
+++ src/chapters/fragments/code/0xff/23_urzas_pendulum.fs
@@ -12,38 +12,29 @@ void main(){
   vec2 ar = vec2(1.,u_res.y/u_res.x);
   float friction = max(0.,1.-(u_time/2.));
   
-  vec2 p = ar*(gl_FragCoord.xy/u_res * 2. -1.);
-  p.y-= 1.;
+  vec2 p = ar*(gl_FragCoord.xy/u_res * 2. -1.);p.y-= 1.;
   
   vec2 m = vec2(-1.,1.)*ar*(u_mouse.xy/u_res*2.-1.);
   vec2 lm = vec2(-1.,1.)*ar*(u_lastMouseDown/u_res*2.-1.);
-  lm.y +=1.;
-  m.y +=1.;
+  lm.y +=1.;m.y +=1.;
 
   float armLength = length(lm);
   vec2 point = normalize(vec2(abs(lm.x), lm.y));
-
-  // point.x = 0.;
-  // point.y = 1.;
-
   vec2 downVec = vec2(0.,1.);
   float d = dot(downVec, point);
-
   float swingMag = acos(d);
 
-  // very top will mean we're pulling the bob as far up as it will go..
   float theta = cos(t) * swingMag * friction;
-
 	vec2 bPos = p + armLength * (vec2(-sin(theta), cos(theta)));
 
-  // place bob under cursor if mouse button is down
-	if(u_mouse.z == 1.){
-		bPos = p + m;
-	}
+	if(u_mouse.z == 1. && u_lastMouseDown.x < -10.){
+    bPos = p + m;
+  }
  
 	float i = step(cSDF(bPos, 0.25), 0.);
+  i += step(cSDF(p, 0.05), 0.);
 
-  gl_FragColor = vec4(i, i ,i, 1.);
+  gl_FragColor = vec4(vec3(i), 1.);
 
   if (abs(p.x) < 0.01 || abs(p.y) < 0.01){
     gl_FragColor = vec4(1.);
