6ea4a0cafebc7ec5c79bd5a95829810d0d257d63
Andor Saga <andor.saga@gmail.com>
2018-05-02 12:36:57 -0400

    dir light working.

--- src/chapters/fragments/code/0xff/20/28_the_wall.fs
+++ src/chapters/fragments/code/0xff/20/28_the_wall.fs
@@ -5,32 +5,29 @@ uniform vec3 u_mouse;
 uniform sampler2D u_texture0;
 uniform sampler2D u_texture1;
 
-void main(){
+void main(){  
   vec2 a = vec2(1., u_res.y/u_res.x);
   vec2 p = a*(gl_FragCoord.xy/u_res*2.-1.);
-
-  float t = u_time*2.;
-  vec2 m = vec2(u_mouse);//(u_mouse.xy/u_res*2.-1.);;
-
+  float t = u_time*.5;
   // p.x += t;
+  float zScale = .5;
+  vec3 m = vec3((u_mouse.xy/u_res*2.)-1., 1.);
+  
+  vec3 dirLight = normalize(m);
+  // vec3 pointLight = 
+
 
   vec2 uv = mod(p, 1.);
-  vec3 dirLight = normalize(vec3(m.x, m.y, 1.));
+  vec4 diffuse = texture2D(u_texture0,uv);
+  vec4 normal = texture2D(u_texture1,uv);
 
+  vec3 n = vec3(normal.xy*2.-1.,normal.z*zScale);
+  n = normalize(n);
 
-
-  float ambientCol = 1.0;
-
-  // get the color for the fragment
-  vec4 diffuse = texture2D(u_texture0, uv);
-  vec4 normal = texture2D(u_texture1, uv);
-
-  vec3 n = normalize(vec3(vec2(normal),1.0));
-  float theta = dot(n,dirLight);
+  float theta = dot(n, dirLight);
 
   vec3 finalCol = vec3(diffuse) * theta;
-
-
+  finalCol = vec3(theta);
   
   gl_FragColor = vec4(finalCol, 1.);
 }
