f89e77f9d021fd8c9af100a59d409788823516b2
Andor Saga <andor.saga@gmail.com>
2018-05-02 15:12:52 -0400

    polish

--- src/chapters/fragments/code/0xff/20/28_the_wall.fs
+++ src/chapters/fragments/code/0xff/20/28_the_wall.fs
@@ -5,29 +5,51 @@ uniform vec3 u_mouse;
 uniform sampler2D u_texture0;
 uniform sampler2D u_texture1;
 
-void main(){  
+void main(){
+  // play area :)
+  float t = u_time*.1;
+  float zScale = .8;
+  float texScale = 2.;
+
+  // color stuff
+  const float shininess = 16.;
+  const float lightPower = .8;
+  vec3 ambientColor = vec3(0.);
+  vec3 diffuseColor = vec3(0.96, 1., 1.);
+  vec3 specColor = vec3(.84, .78, .45);
+
   vec2 a = vec2(1., u_res.y/u_res.x);
   vec2 p = a*(gl_FragCoord.xy/u_res*2.-1.);
-  float t = u_time*.5;
-  // p.x += t;
-  float zScale = .5;
-  vec3 m = vec3((u_mouse.xy/u_res*2.)-1., 1.);
+  p /= texScale;
+
+  vec3 m = vec3(a*(u_mouse.xy/u_res*2.)-1., 1.);
+  m /= texScale;
   
-  vec3 dirLight = normalize(m);
-  // vec3 pointLight = 
+  vec3 lightToFrag = m - vec3(p.x, -p.y, -1.);
+  float lightDist = length(lightToFrag);
+  lightDist = lightDist*lightDist;
 
+  lightToFrag = normalize(lightToFrag);
 
-  vec2 uv = mod(p, 1.);
-  vec4 diffuse = texture2D(u_texture0,uv);
-  vec4 normal = texture2D(u_texture1,uv);
-
-  vec3 n = vec3(normal.xy*2.-1.,normal.z*zScale);
+  vec2 uv = mod(vec2(p.x,-p.y), 1.);
+  vec4 diffuseMap = texture2D(u_texture0,uv);
+  vec4 normalMap = texture2D(u_texture1,uv);
+  vec3 n = vec3(normalMap.xy*2.-1.,normalMap.z*zScale);
   n = normalize(n);
-
-  float theta = dot(n, dirLight);
-
-  vec3 finalCol = vec3(diffuse) * theta;
-  finalCol = vec3(theta);
   
+  float lambertian = dot(n, lightToFrag);
+
+  // SPEC
+  vec3 reflectDir = reflect(-lightToFrag, n);
+  float specAngle = dot(reflectDir, vec3(0., 0., 1.));
+  // float specAngle = dot(reflectDir, vec3(0., 0., -1.));
+  float specular = pow(specAngle, shininess*5.0);
+  
+  vec3 ambCalc = ambientColor;
+  vec3 diffCalc = vec3(diffuseMap) * lambertian * diffuseColor * (lightPower/lightDist);
+  vec3 specCalc = vec3(diffuseMap) * specular * specColor * (lightPower/lightDist)/1.1;
+
+  vec3 finalCol = ambCalc + diffCalc + specCalc;
+
   gl_FragColor = vec4(finalCol, 1.);
 }
