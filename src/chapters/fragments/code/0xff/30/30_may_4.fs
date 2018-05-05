a407867339e89202ad201a650e82f52a9cb57659
Andor Saga <andor.saga@gmail.com>
2018-05-05 14:02:11 -0400

    31 greed.

--- src/chapters/fragments/code/0xff/30/30_may_4.fs
+++ src/chapters/fragments/code/0xff/30/30_may_4.fs
@@ -1,18 +1,18 @@
 precision mediump float;
 uniform vec2 u_res;
 #define PI 3.141592658
-float arc(vec2 p, float w, vec2 ra, float len, float theta){
-  return (1.-step(sin(PI*mod(theta+atan(p.y,p.x),PI/3.)),w))
- * step(len,ra.y) * step(ra.x,len);
+float a(vec2 p, float w, vec2 ra, float len, float theta){
+  return (smoothstep(w,w+.04,
+      sin(PI*mod(theta+atan(p.y,p.x),PI/3.))))
+      * step(len,ra.y) * step(ra.x,len);
 }
-
 void main(){
-	vec2 a = vec2(1., u_res.y/u_res.x);
-	vec2 p = a*( gl_FragCoord.xy/u_res*2.-1.);
-	float i = step(length(p)-0.35,0.);
-	float len = length(p);
-	i += arc(p, 0.1,  vec2(0.85, 0.9), len, PI/2.);
-	i += arc(p, 0.7,  vec2(0.8, 0.85), len, PI/2.);
-	i += arc(p, 0.95, vec2(0., 0.7), len, 0.);
-	gl_FragColor = vec4(vec3(i), 1.);
+  vec2 ar = vec2(1., u_res.y/u_res.x);
+  vec2 p = ar*(gl_FragCoord.xy/u_res*2.-1.);
+  float i = 1.-smoothstep(0., 0.01,length(p)-.35);
+  float len = length(p);
+  i += a(p, .1, vec2(.85, .9),  len, PI/2.);
+  i += a(p, .7, vec2(.80, .85), len, PI/2.);
+  i += a(p, .85,vec2(.00, .7),  len, 0.);
+  gl_FragColor = vec4(vec3(i), 1.);
 }
--- src/chapters/fragments/code/0xff/30/3x_check.fs
+++ src/chapters/fragments/code/0xff/30/3x_check.fs
@@ -17,22 +17,50 @@ void main(){
   vec3 m = vec3(a*(u_mouse.xy/u_res*2.-1.), 1.);  
   float i;
   
-  float tileSize = 0.5;
+  float tileSize = 1.;
   float morterSize = tileSize/10.;
 
- 
   // p.x += xOffset;
 
   vec3 l = vec3(0., 0., 1.);
+  
+  // 0..PI
+  float Ix = (mod(p.x, tileSize)/tileSize) * PI;
+  float Iy = (mod(p.y, tileSize)/tileSize) * PI;
+
+  float ox = Ix;
+  float oy = Iy;
+
+  // float angle = .5;
+  // Ix = Ix / PI;
+  // Ix *= angle;
+  // float delta = 1. - angle;
+  // Ix += delta/2.;
+  // Ix *= PI;
 
   //  create cylinders, just to debug
-  float I = (mod(p.x, tileSize )/tileSize) * PI;
-  vec3 n = vec3(-cos(I), 0., sin(I));
+  vec3 n = vec3(-cos(Ix), 0., sin(Ix));
 
-  if(I > PI/10. && I < PI - PI/10.){
-  	n = vec3(0., 0., 1.);
+
+  if(Ix < 0.2 && Ix < 2.8 &&  Iy < 0.2 && Iy < 2.8 ){
+    n  = vec3(0. , 0., 0.);
   }
+  else if(Iy < 0.1 && Iy < 3. ){
+    n  = vec3(0. , 0., 1.);
+  }
+            // vec3(-cos(Iy), 0., sin(Iy)) , 0.5);
 
+
+
+
+
+
+
+
+  if(ox > PI/10. && ox < PI - PI/10. && 
+    oy > PI/10. && oy < PI - PI/10. ){
+  	//n = vec3(0., 0., 1.);
+  }
   n = normalize(n);
 
  
@@ -41,11 +69,9 @@ void main(){
   lightDist = lightDist*lightDist;// ?????
   lightToFrag = normalize(lightToFrag);
 
-
-  float xOffset = step(tileSize, mod(p.y, tileSize*2.));
-
-  i = step(tileSize, mod(p.x, tileSize*2.));
-  vec3 tileColor = vec3(i- xOffset);
+  float xOffset = tileSize * step(tileSize, mod(p.y, tileSize*2.));
+  i = step(tileSize, mod(p.x+xOffset, tileSize*2.));
+  vec3 tileColor = vec3(i);
 
 
 
@@ -58,7 +84,7 @@ void main(){
   vec3 specCalc = specColor * vec3(1.) * specular * (lightPower/lightDist);
 
   vec3 finalCol =	0. + 
-  					ambientCalc + 
+  					// ambientCalc + 
   					diffuseCalc + 
   					specCalc;
   					0.;
