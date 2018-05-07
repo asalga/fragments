60226bedd4c833452700193bc942b2405819054a
Andor Saga <andor.saga@gmail.com>
2018-05-07 19:43:20 -0400

    raycast.

--- src/chapters/fragments/code/0xff/40/40_raycast.fs
+++ src/chapters/fragments/code/0xff/40/40_raycast.fs
@@ -1,34 +1,27 @@
-// Simple RayCasting
 precision mediump float;
+
+// resource 
+// https://andorsaga.wordpress.com/2014/01/22/understanding-raycasting-step-by-step/
 
 #define PI 3.141592658
 #define DEG_TO_RAD PI/180.
 #define TAU PI*2.
 
-#define FOV 45.
-#define HALF_FOV FOV/2.
+#define FOV 45.0
 
 uniform vec2 u_res;
 
 // TODO:
-// - review theory
 // - review DDA algorithm
 // - add 1 wall
 // - use texture for level data
 // - add kb input
-// define player fov
 
 // Theory:
 // Cast 'width' number of rays into the scene
 // But since we're working with a frag shader
 // we'll have some added benefits of working with Y
 // so later on texture mapping will be straightforward
-//
-
-// For each fragment, what is our 'x' scanline?
-
-// Q?
-// How to get closest edge?
 
 
 float background(vec2 p){
@@ -43,10 +36,11 @@ void main(){
 
 	// The FOV determines the angle the rays shoot into the scene.
 	// the center ray then will be the player's direction.
-	float FOV = 0.5;
+	// float FOV = .5;
 
+	vec2 playerPos = vec2(0.);
 	vec2 playerDir = vec2(0.,1.);
-	vec2 playerRight = vec2(1., 0.);
+	vec2 playerRight = vec2(1.,0.);
 	// vec2 right = vec2(-forward.y, forward.x);
 
 	// We need to calculate the camera line magnitude
@@ -58,7 +52,7 @@ void main(){
 
 	// andorsaga.files.wordpress.com/2014/01/raycast-distortion-fix.jpg
 
-	float camRatio = (HALF_FOV * DEG_TO_RAD);
+	float camRatio = (FOV/2. * DEG_TO_RAD);
 
 	// Which slice we are working with?
 	float sliceXIdx = gl_FragCoord.x;
@@ -69,13 +63,41 @@ void main(){
 	// triangle viewing area.
 
 	// Sweep across the viewport, scaling the right vector
+	// Note, when p=0, the ray is collinear with playerDir
 	vec2 rayDir = vec2(playerDir + (playerRight * camRatio) * p);
 
+	// Find the closest edge, if that edge has a cell filled in, it's a wall,
+	// otherwise it's not
+
+	// What is the mag from one x edge to the next x edge?
+	// The 'steeper' the rayDir, the greater the value
+	// magnitude of how much to 'step' x and y each tile
+	// For x, note that the base length of the triangle
+	// formed from one step to the other will have length = 1
+
+	float dirStepY;
+
+	// We're trying to get the length of the ray from one x edge
+	// to the next
+	// We know the x distance is 1
+	// Since our ray is normalized, the x will be less than 1 meaning
+	// we can 'stretch' it so it reaches the next x-edge. Of course,
+	// we'll need to do the same for the y to maintain the ray direction
+
+	float magBetweenEdgeX = length(rayDir * (1./rayDir.x));
+	float magBetweenEdgeY = length(rayDir * (1./rayDir.y));
 
 
 
 
+
+
+	// Run the DDA
+	// ...
 	
+
+
+
 	float background = background(p);
 
 	float i =   0.0 +
