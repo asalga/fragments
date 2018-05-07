0739957c48160d1eeab3fd36bae5aaafebe99336
Andor Saga <andor.saga@gmail.com>
2018-05-07 10:07:13 -0400

    33 & raycast.

---
+++ src/chapters/fragments/code/0xff/30/33_thirty-trhee.fs
@@ -0,0 +1,29 @@
+precision mediump float;
+uniform vec2 u_res;
+uniform float u_time;
+#define PI 3.141592658
+float random (vec2 st) {
+  return fract(sin(dot(st.xy,vec2(12.9898,178.233)))*43758.5453123);
+}
+float cSDF(vec2 p, float r){
+  return length(p) - r;
+}
+void main(){
+  vec2 a = vec2(1.,u_res.y/u_res.x);
+  vec2 p = 1.2 * a * (gl_FragCoord.xy / u_res * 2. -1.);
+  float t = mod(u_time, 2.*PI);
+  vec3 s = vec3(cos(t), .0, sin(t));
+  float z = sqrt(1. - pow(p.x,2.) - pow(p.y,2.));
+  vec3 v = normalize(vec3(p.x, p.y, z));
+  vec3 i = vec3(dot(v, s));
+  				// smoothstep(.48, .49, 1.-cSDF(p, .5)) -
+  				// smoothstep(.38, .39, 1.-cSDF(p, .37)));
+
+  float r = random(p);
+  i.z = i.y = i.x = step(i.x,r);
+
+
+  gl_FragColor = vec4(i, 1.);
+}
+
+
