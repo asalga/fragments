0e6ef5f910845bc25fcca244b9da3b4790472900
Andor Saga <andor.saga@gmail.com>
2018-05-08 23:01:15 -0400

    .

--- src/chapters/fragments/code/0xff/30/34.fs
+++ src/chapters/fragments/code/0xff/30/34.fs
@@ -1,6 +1,7 @@
 precision mediump float;
 uniform vec2 u_res;
 uniform float u_time;
+uniform vec3 u_mouse;
 
 float cSDF(vec2 p, float r){
   return length(p)-r;
@@ -8,12 +9,22 @@ float cSDF(vec2 p, float r){
 
 void main(){
   vec2 a = vec2(1., u_res.y/u_res.x);
-  vec2 p = gl_FragCoord.xy/u_res*2.-1.;
+  vec2 p = a* (gl_FragCoord.xy/u_res*2.-1.);
+  vec2 m = vec2(-1., a.y) * ((u_mouse.xy/u_res)*2.-1.);
+  float t = u_time;
   
-  float c1 = cSDF(p, 0.5);
+  float r1 = 0.15;
+  vec2 c1p = m;
+  float c1 = step(cSDF(p+m, r1), 0.);
 
-  // circle circle intersection
+  float r2 = 0.5;
+  vec2 c2p = vec2(cos(t), sin(t));
+  float c2 = step(cSDF(p+c2p, r2), 0.);
 
-  float i = 1.;
-  gl_FragColor = vec4(vec3(c1), 1.);
+  float len = length(c1p-c2p);
+  float i = abs( step(len, r1+r2)- (c1 + c2));
+
+  
+
+  gl_FragColor = vec4(vec3(i), 1.);
 }
