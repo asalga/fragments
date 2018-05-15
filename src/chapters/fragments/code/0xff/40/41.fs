65ae34417b2d6f3ad51cb920207f3986376cacef
Andor Saga <andor.saga@gmail.com>
2018-05-15 11:13:46 -0400

    41

--- src/chapters/fragments/code/0xff/40/41.fs
+++ src/chapters/fragments/code/0xff/40/41.fs
@@ -1,7 +1,8 @@
 precision mediump float;
 uniform vec2 u_res;
 uniform float u_time;
-#define PI 3.14159
+#define PI 3.141592658
+#define TAU 2.*PI
 
 float cSDF(vec2 p, float r){
   return length(p)-r;
@@ -9,11 +10,26 @@ float cSDF(vec2 p, float r){
 mat2 r2d(float a){
   return mat2(cos(a),-sin(a),sin(a),cos(a));
 }
+
+
+// -PI  ..  PI
+// We have values that range from -PI to PI
+// we need to divide that into n sections
+// find the midpoint
+// 
+
+
+
 void main(){
   vec2 a = vec2(1., u_res.y/u_res.x);
   vec2 p = a*(gl_FragCoord.xy/u_res*2.-1.);
   float r = 0.1;
-  float count = 6.;
+  float count = 4.;
+  float sliceSize = TAU/count;
+
+
+
+
 
   float i;
   float theta = atan(p.y,p.x);
@@ -28,13 +44,8 @@ void main(){
 
   i = mod(normTheta, PI/2.);//*2.-1.;
 
-  // take curr point and find out distance
-
   // get distance from curr point to closest point
   
-  // p - closest;
-  
-  // snap vector
   // vec2 v = vec2(1., 0.) * r2d(mod(theta, 0.9));
   float n = count * mod(theta, PI);
   vec2 v = vec2(1., 0.) * r2d(n);
@@ -47,11 +58,10 @@ void main(){
   // }
   // i = step(cSDF(p - v, 0.4), 0.);
 
-  float tt =  (atan(p.y,p.x)+PI)/2./PI;
+  float tt = (atan(p.y,p.x)+PI)/2./PI;
   float C = 4.;
   float snapTheta = 0.;
   float H = (1./C) /2.;
-
 
   // -PI  ..  PI
   float A = atan(p.y, p.x);
@@ -64,15 +74,23 @@ void main(){
   	v = vec2( cos(snapTheta), sin(snapTheta));
   }
 
+  ////////////////////////
+  // 'start' at -PI and increase however much we need
+  float T = atan(p.y,p.x);
+  float COUNT = 2.;
+  float idx = floor(((T+PI)/PI) * COUNT);
+  float snapy = -PI + (idx * sliceSize);
+  snapy += sliceSize/2.;
+  float R = 0.8;
+  v = vec2( cos(snapy)*R, sin(snapy)*R);
+
 
 
   // if(tt > 0. && tt < 1./C)  {
   // 	// v = vec2(-.5, -.5);
   // 	snapTheta = 20.;
 
-  // 	// 0 .. 1/4
-  // 	// 
-
+   
   // 	 //(1./C + (H)) *PI;
   // 	// snapTheta = -1.;
   // 	v = vec2( -cos(snapTheta), sin(snapTheta));
@@ -108,7 +126,7 @@ void main(){
   // }
   // i = step(cSDF(p - v, 0.4), 0.);
 
-  i = step(cSDF(p - v, 0.134), 0.);
+  i = cSDF(p - v, 0.1);
   // i = step(cSDF(p - v, 0.134), 0.);
 
    
