cd8f54a9a203216ae9c5a2b41d4e1ecea5d78e6b
Andor Saga <andor.saga@gmail.com>
2018-05-16 15:22:10 -0400

    42.

--- src/chapters/fragments/code/0xff/40/42.fs
+++ src/chapters/fragments/code/0xff/40/42.fs
@@ -3,36 +3,47 @@ uniform vec2 u_res;
 uniform float u_time;
 #define PI 3.141592658
 #define TAU 2.*PI
-#define COS_30 0.8660256249
+#define COS_30 .8660256249
+#define DEG_TO_RAD PI/180.
+
+float cSDF(vec2 p, float r){
+  return length(p)-r;
+}
 mat2 r2d(float a){
   return mat2(cos(a),-sin(a),sin(a),cos(a));
 }
 float triSDF(vec2 p, float s, float sc){
   vec2 a = abs(p);
   float distToSide = a.x * COS_30;
-  float u = p.y * sc * 0.6;
+  float u = p.y * sc * 1.;
   return max(distToSide + u, -u) - s;
 }
 
 void main(){
   vec2 a = vec2(1., u_res.y/u_res.x);
   vec2 p = a*(gl_FragCoord.xy/u_res*2.-1.);
-  float i;
-  float count = 6.;  
-  float sliceSize = TAU/count;
+  float count = 3.;  
+  float sliceSize = PI/count;
+  float CC = 360./(count*2.);
   float theta = atan(p.y,p.x);
-  float idx = floor( ((theta+PI) / TAU) * count);
+  float i;
+
+  i = step(cSDF(p, 1.), 0.);
+
+  //                   0..TAU      0..1   [0,1,..c-1]
+  float idx = floor( ((theta+PI) / PI) * count);
+
   float snapped = -PI + (idx * sliceSize) + sliceSize/2.;
-  vec2 v = vec2(cos(snapped), sin(snapped)) * 0.8;
+  vec2 v = vec2(cos(snapped), sin(snapped)) * .8;
 
-  // need to rotate triangle a certain angle
-  // depending on what section they are in.
-  p-=v;
+  float T = (1.+(sin(u_time)))/2.;
 
-  // i = step(udRoundBox(p, vec2(.01, .01), 0.05), 0.);
-  i = step(triSDF(p,0.05, 0.4), 0.);
-  // i = triSDF(p,0.15, 0.5);
+  // remap 0..2 to 1..-1
+  float triRot = (idx -1.) * -(DEG_TO_RAD*CC);
+  p = (p-v) * r2d(triRot);
 
-  // float TEST = (snapped+PI)/TAU;
-  gl_FragColor = vec4(vec3(i,i, i),1.);
+  // move outwards
+  p = p - (v*r2d(triRot) * T); 
+  i *= step(triSDF(p, .4, .5), 0.);
+  gl_FragColor = vec4(vec3(i,i,i),1.);
 }
