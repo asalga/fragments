28d43f696aa4beb98a8ecf1fcde03b1b95986673
Andor Saga <andor.saga@gmail.com>
2018-05-18 13:27:48 -0400

    \.

--- src/chapters/fragments/code/0xff/40/44_shapeshift.fs
+++ src/chapters/fragments/code/0xff/40/44_shapeshift.fs
@@ -1,15 +1,14 @@
 // Shapeshifting & timetravelling
 precision mediump float;
 #define PI 3.141592658
-#define E 0.001
 #define SZ .35
 uniform vec2 u_res;
 uniform float u_time;
 mat2 r2d(float a){
   return mat2(cos(a),sin(a),sin(a),-cos(a));
 }
-float cSDF(vec2 p, float r){
-  return length(p) - r;
+float ringSDF(vec2 p, float r, float w){
+  return abs(length(p) - (r*.5)) - w;
 }
 float rSDF(vec2 p, vec2 size) {  
   vec2 d = abs(p) - size;
@@ -18,19 +17,15 @@ float rSDF(vec2 p, vec2 size) {
 void main(){
   vec2 a = vec2(1., u_res.y/u_res.x);
   vec2 p = a*(gl_FragCoord.xy/u_res*2.-1.);
-  float i;
   float t = u_time*4.;
   float theta = sin(t)*PI/8.;
-  vec2 bPos = vec2(sin(-theta)*a.y,cos(theta)-1.);
-  vec2 bob = (p + bPos) * r2d(theta);
-  float c = cSDF(bob, SZ);
-  float r = rSDF(bob, vec2(SZ, SZ));
-
-  float state = (sin(t)+1.)/2.;
-  i = smoothstep(0.01, 0.001, mix(c,r,state));
-
+  vec2 bPos = (p+vec2(sin(-theta)*a.y,cos(theta)-1.))
+  			  * r2d(theta);
+  float c = ringSDF(bPos,SZ*2., .02);
+  float r = rSDF(bPos,vec2(SZ));
+  float i = smoothstep(0.01, 0.001,mix(c,r,(sin(t)+1.)/2.));
   p.y -= a.y;
   p *= r2d(theta);
-  i += step(rSDF(p, vec2(0.01, a.y)), 0.);
+  i += smoothstep(0.01, 0.001, rSDF(p, vec2(0.005, a.y-0.3)));
   gl_FragColor = vec4(vec3(i), 1.);
 }
