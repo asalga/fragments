d88139c088371563fef515017ff0f60e84d9f41f
Andor Saga <andor.saga@gmail.com>
2018-05-18 13:21:05 -0400

    \.

--- src/chapters/fragments/code/0xff/40/44_shapeshift.fs
+++ src/chapters/fragments/code/0xff/40/44_shapeshift.fs
@@ -1,7 +1,13 @@
+// Shapeshifting & timetravelling
 precision mediump float;
+#define PI 3.141592658
+#define E 0.001
+#define SZ .35
 uniform vec2 u_res;
 uniform float u_time;
-#define SZ .5
+mat2 r2d(float a){
+  return mat2(cos(a),sin(a),sin(a),-cos(a));
+}
 float cSDF(vec2 p, float r){
   return length(p) - r;
 }
@@ -14,12 +20,17 @@ void main(){
   vec2 p = a*(gl_FragCoord.xy/u_res*2.-1.);
   float i;
   float t = u_time*4.;
+  float theta = sin(t)*PI/8.;
+  vec2 bPos = vec2(sin(-theta)*a.y,cos(theta)-1.);
+  vec2 bob = (p + bPos) * r2d(theta);
+  float c = cSDF(bob, SZ);
+  float r = rSDF(bob, vec2(SZ, SZ));
 
-  float c = cSDF(p, SZ);
-  float r = rSDF(p, vec2(SZ, SZ));
+  float state = (sin(t)+1.)/2.;
+  i = smoothstep(0.01, 0.001, mix(c,r,state));
 
-  float s = (sin(t)+1.)/2.;
-  i = smoothstep(0.01, 0.001, mix(c,r,s));
-
+  p.y -= a.y;
+  p *= r2d(theta);
+  i += step(rSDF(p, vec2(0.01, a.y)), 0.);
   gl_FragColor = vec4(vec3(i), 1.);
 }
