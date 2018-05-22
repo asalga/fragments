3afbd326351b9e68d6d188983da7325ffb771732
Andor Saga <andor.saga@gmail.com>
2018-05-22 10:37:35 -0400

    his shadow template.

--- src/chapters/fragments/code/0xff/40/48_squiggler.fs
+++ src/chapters/fragments/code/0xff/40/48_squiggler.fs
@@ -1,7 +1,7 @@
 // 21 - thought of water reprise
 precision mediump float;
 
-uniform vec2 u_tracking;
+uniform vec2 ;
 uniform vec2 u_res;
 uniform float u_time;
 
@@ -18,18 +18,16 @@ float cInter(vec2 p, float r, float i){
 void main(){
   vec2 a = vec2(1., u_res.y/u_res.x);
   vec2 p = a*(gl_FragCoord.xy/u_res*2.-1.);
+  float t = u_time;
 
-  float t = u_time * 3.;
-  float shfit = cos(t+p.x*(u_tracking.x+0.5)*10.)/10.;
-  float tr = ((2.*u_tracking.y)-1.)/3.;
+  // p = mod(p, vec2(0.2, 0.3));
 
-  vec2 _t1 = vec2(-tr, -.35 + shfit);
-  float _1 = cInter(p+_t1 , 1.2, 1.1);
-  vec2 _t2 = vec2(tr, .35 + shfit);
-  float _2 = cInter(p+_t2 , 1.2, 1.1);
-  vec2 _t3 = vec2(0., shfit);
-  float _3 = cInter(p+_t3 , 1.2, 1.1);
+  float shfit = cos( t + p.x * 2.)/10.;
+  float tr = ((2.)-1.)/3.;
 
-  float i = _1 + _2 + _3;
+  // vec2 _t1 = vec2(-tr, shfit);
+  float _1 = cInter(p , 1.2, 1.01);
+
+  float i = _1;
   gl_FragColor = vec4(vec3(i), 1.);
 }
