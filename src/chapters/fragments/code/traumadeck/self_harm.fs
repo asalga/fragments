80163e76d90fbfa92cec33e57044146f7d606ce2
Andor Saga <andor.saga@gmail.com>
2018-05-23 19:02:37 -0400

    .

--- src/chapters/fragments/code/traumadeck/self_harm.fs
+++ src/chapters/fragments/code/traumadeck/self_harm.fs
@@ -19,14 +19,15 @@ float triSDF(vec2 p, float s, float sc){
   float u = p.y * sc;
   return max(distToSide + u, -u) - s;
 }
-
 float arrow(vec2 p){
   float arrowLen = 0.2;
   float tri = step(triSDF(p, 0.05, 0.5), 0.);
   float rect = step(rectSDF(p+ vec2(0., arrowLen), vec2(0.01, 0.2)), 0.);
   return tri + rect;
 }
-
+mat2 r2d(float a){
+  return mat2(cos(a),-sin(a),sin(a),cos(a));
+}
 void main(){
   vec2 a = vec2(1., u_res.y/u_res.x);
   vec2 p = a*(gl_FragCoord.xy/u_res*2.-1.);
@@ -36,12 +37,19 @@ void main(){
   float pupil = step(cSDF(p, 0.05), 0.);
   float outerCirc = step(ringSDF(p, r, 0.01), 0.);
 
+  float rot = 0.8;
+  float nudge = 0.18;
   // subtract a bit so triangle touches
-  vec2 arr1Pos = p + vec2(0., r-0.15);
+  vec2 arr1Pos = p * r2d(rot) + vec2(0., r-nudge);
   float arrow1 = arrow(arr1Pos);
 
+  vec2 arr2Pos = p * r2d(0.) + vec2(0., r-nudge);
+  float arrow2 = arrow(arr2Pos);
 
-  i += pupil + outerCirc + arrow1;
+  vec2 arr3Pos = p * r2d(-rot) + vec2(0., r-nudge);
+  float arrow3 = arrow(arr3Pos);
+
+  i += pupil + outerCirc + arrow1 + arrow2 + arrow3;
 
   gl_FragColor = vec4(vec3(i),1.);
 }
