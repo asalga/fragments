e33970f79a69d978f37c6b32ae0b2e1db66bde52
Andor Saga <andor.saga@gmail.com>
2018-05-30 13:37:53 -0400

    59

--- src/chapters/fragments/code/0xff/60/6x_ray_trace.fs
+++ src/chapters/fragments/code/0xff/60/6x_ray_trace.fs
@@ -4,13 +4,10 @@ uniform vec2 u_res;
 uniform float u_time;
 void main(){
   vec2 a = vec2(1., u_res.y/u_res.x);
-  vec2 p = a * (gl_FragCoord.xy/u_res*2.-1.);
-  vec3 col;
-	float t0, t1; // intersections
-
-  vec3 spherePos = vec3(0., 0., -5.);
+  vec2 p = 1.5* (a * (gl_FragCoord.xy/u_res*2.-1.));
+  // no perspective, changing distance = no foreshortening
+  vec3 spherePos = vec3(cos(u_time), sin(u_time), -2.);
   float rad = 1.;
-
   vec3 rayOrigin = vec3(p.x, p.y, 0.);
   vec3 rayDir = vec3(0., 0, -1.);
 
@@ -21,15 +18,18 @@ void main(){
   float tca = dot(rayToSphere, rayDir);
 
   // if negative, no intersection
-  // rayDirection was pointing in opposite direction
-  if(tca < 0.){col.r = 0.5;}
+  if(tca < 0.){discard;}
 
   // d = sqrt(rayToSphere² - tca²)
-  float d = sqrt(dot(rayToSphere,rayToSphere) - dot(tca,tca));	
+  float d = sqrt(dot(rayToSphere,rayToSphere)-dot(tca,tca));	
 
   // thc = sqrt(r² - d²)
-	float thc = sqrt(pow(rad,2.) - pow(d,2.));
+  float thc = sqrt(pow(rad,2.)-pow(d,2.));
 
+  // how much we'd have to scale the ray to hit the sphere
+  float t0 = tca - thc;
 
+  // basic coloring
+  vec3 col = vec3(2. - t0);
   gl_FragColor = vec4(col,1.);
 }
