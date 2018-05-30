1d2abfc7b365bda359667a639e9954c2988ef3e1
Andor Saga <andor.saga@gmail.com>
2018-05-30 10:40:28 -0400

    58

--- src/chapters/fragments/code/0xff/60/6x_ray_trace.fs
+++ src/chapters/fragments/code/0xff/60/6x_ray_trace.fs
@@ -1,18 +1,15 @@
 // 6x - "ray trace" method from scratchapixel
 precision mediump float;
-
 uniform vec2 u_res;
 uniform float u_time;
-
 void main(){
   vec2 a = vec2(1., u_res.y/u_res.x);
   vec2 p = a * (gl_FragCoord.xy/u_res*2.-1.);
   vec3 col;
-  float t = u_time;
 	float t0, t1; // intersections
 
   vec3 spherePos = vec3(0., 0., -5.);
-  float sphereRad = 1.;
+  float rad = 1.;
 
   vec3 rayOrigin = vec3(p.x, p.y, 0.);
   vec3 rayDir = vec3(0., 0, -1.);
@@ -20,12 +17,18 @@ void main(){
   // Need to t0 w/ tca and thc
   vec3 rayToSphere = spherePos - rayOrigin;
 
-  // Projection
+  // Project vector rayToSphere onto rayDir
   float tca = dot(rayToSphere, rayDir);
 
-  if(tca < 0.){
-  	col.r = 0.5;
-  }
+  // if negative, no intersection
+  // rayDirection was pointing in opposite direction
+  if(tca < 0.){col.r = 0.5;}
+
+  // d = sqrt(rayToSphere² - tca²)
+  float d = sqrt(dot(rayToSphere,rayToSphere) - dot(tca,tca));	
+
+  // thc = sqrt(r² - d²)
+	float thc = sqrt(pow(rad,2.) - pow(d,2.));
 
 
   gl_FragColor = vec4(col,1.);
