1f61c9d6742d8896c1579ab6eb629ecad392cfd8
Andor Saga <andor.saga@gmail.com>
2018-06-02 13:50:57 -0400

    59

--- src/chapters/fragments/code/0xff/50/59_no_bounce_no_play.fs
+++ src/chapters/fragments/code/0xff/50/59_no_bounce_no_play.fs
@@ -1,10 +1,20 @@
 // 58 - "No bounce, no play"
-// add Y direction
-// add random positions
 precision mediump float;
 uniform vec2 u_res;
+#define PI 3.141592658
 uniform float u_time;
-#define NUM_BALLS 1
+#define NUM_BALLS 10
+highp float rand(vec2 co){// from byteblacksmith.com
+  highp float a = 12.9898;
+  highp float b = 78.233;
+  highp float c = 43758.5453;
+  highp float dt= dot(co.xy ,vec2(a,b));
+  highp float sn= mod(dt,3.14);
+  return fract(sin(sn) * c);
+}
+// float sdCircle(vec2 p, float r){
+  // return length(p) - r;
+// }
 float circle(vec2 p, float r){
   return smoothstep(0.01, 0.001, length(p) - r);
 }
@@ -14,13 +24,17 @@ void main(){
   vec3 metaBalls[NUM_BALLS];
   float i;
   for(int it = 0; it < NUM_BALLS; it++){
-	vec2 pos = vec2(u_time, 0.);
-    // 8 other "screens" surround the canvas
-    float screenIdx = floor(mod(pos.x, 2.));// 0..1
- 	float dir = screenIdx*2.-1.;// remap to  -1..+1
-  	vec2 finalPos = vec2(  (1.-screenIdx) + dir * mod(pos.x, 1.), 0.);
-	finalPos.x = (finalPos.x-.5) *2.;
-    i = circle(p + finalPos, .25);
+  	float f_it = float(it);
+    vec2 ballVel = vec2(rand(vec2(f_it+1.734))*2.-1.,
+    					rand(vec2(f_it-1.734))*2.-1.);
+	vec2 pos = vec2(0.5) + ballVel * vec2(u_time);
+    // float screenIdx = floor(mod(pos.x, 2.));// 0..1
+    vec2 screenIdx = floor(mod(pos, vec2(2.)));// 0..1
+ 	vec2 dir = screenIdx*2.-1.;// remap to  -1..+1
+  	vec2 finalPos = vec2((1.-screenIdx) + dir * mod(pos, 1.));
+	finalPos = p + (finalPos-.5)* 2.;
+    i += circle(finalPos, .25);
   }
+  i = 1. - step(mod(i, 2.), 0.);// invert on odd/even
   gl_FragColor = vec4(vec3(i), 1.);
 }
