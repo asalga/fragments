e8bb4f37723de4fb732cf555ed142b207bcafe33
Andor Saga <andor.saga@gmail.com>
2018-06-04 12:39:16 -0400

    61

--- src/chapters/fragments/code/0xff/60/61_spaces.fs
+++ src/chapters/fragments/code/0xff/60/61_spaces.fs
@@ -2,7 +2,7 @@ precision mediump float;
 #define PI 3.14159
 uniform vec2 u_res;
 uniform float u_time;
-
+#define DEPTH 5
 float sdCircle(vec2 p, float r){
   return length(p) - r;
 }
@@ -21,24 +21,14 @@ void main(){
   vec2 p = a * (gl_FragCoord.xy/u_res*2.-1.);
   float i;
   float thickness = 0.005;
-  // vec2 p0 = mod(p, 1.);
-  // float p0sz = 1.;
-  // vec2 p1 = mod(p, 0.5);
-  // float p1sz = 0.5;
+  i += ring(p,2.,thickness);
 
-    // vec2 _p = mod(p,sz);
-    // vec2 c = vec2(-sz/2.);
-    i += ring(p,2.,thickness);
-
-
-  // i += ring(p0+vec2(-.5, -.5), p0sz, 0.01);
-  // i += ring(p1+vec2(-.5, -.5), p1sz, 0.01);
-  for(int it = 1; it < 3; it++){
-	float _i = float(it);
-  	float sz = 1./_i;
-    vec2 _p = mod(p+ vec2(0.,a.y/2.), vec2(sz,0.));
+  float sz = 1.;
+  for(int it = 0; it < DEPTH; it++){
+    vec2 _p = mod(p + vec2(0.,a.y/2.), vec2(sz,0.));
     vec2 c = vec2(-sz/2., -a.y/2.);
     i += ring(_p+c,sz,thickness);
+    sz *= .5;
   }
 
   gl_FragColor = vec4(vec3(i),1);
