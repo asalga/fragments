abe20f4bbebe3899abd5c11d16c7168c0ec54689
Andor Saga <andor.saga@gmail.com>
2018-06-05 11:11:47 -0400

    62

--- src/chapters/fragments/code/0xff/60/62.fs
+++ src/chapters/fragments/code/0xff/60/62.fs
@@ -1,12 +1,13 @@
 precision mediump float;
-
+#define TAU 3.141592658*2.
 uniform vec2 u_res;
 uniform float u_time;
-
 void main(){
   vec2 a = vec2(1., u_res.y/u_res.x);
   vec2 p = a * (gl_FragCoord.xy/u_res*2.-1.);
-  float i;
+  p.x = (p.x+1.)*.5;// adjust x
 
+  float i = step(p.y, sin(p.x*TAU));
+  
   gl_FragColor = vec4(vec3(i),1.);
 }
