5a04bf44f0bb1af5b6ff44874e6b96d0276cae6c
Andor Saga <andor.saga@gmail.com>
2018-06-06 11:49:02 -0400

    \.

--- src/chapters/fragments/code/0xff/60/63.fs
+++ src/chapters/fragments/code/0xff/60/63.fs
@@ -10,14 +10,18 @@ float sdTri(vec2 p, float s, float sc){
   float u = p.y * sc;
   return max(distToSide + u, -u) - s;
 }
-float easeInOutBack (float t, float b, float c, float d){
+float easeInOutBack(float t, float b, float c, float d){
   float s = 1.70158;
   if ((t /= d/2.) < 1.){
   	return c/2.*(t*t*(((s*=(1.525))+1.)*t - s)) + b;
   }
   return c/2.*((t-=2.)*t*(((s*=(1.525))+1.)*t + s) + 2.) + b;
 }
- 
+//gsgd.co.uk/sandbox/jquery/easing/jquery.easing.1.3.js
+float circ(float t, float b, float c, float d) {
+  return -c * (sqrt(1. - (t /= d)*t) - 1.) + b;
+}
+
 float sdCircle(vec2 p, float r){
   return length(p)-r;
 }
@@ -42,25 +46,24 @@ void main(){
   float t = u_time * 1.0;
   vec2 colorInv = p;
 
-  // b: starting position
-  // c: amount of change (end - start)
-  // d: total animation time/steps
-
   // rotate only every second
   float modt = step(1., mod(t, 2.));
-  float e = easeInOutBack( modt * fract(t), 0., .05, .5);
+  // float e = easeInOutBack( modt * fract(t), 0., .05, .5);
+  float e = circ( modt * fract(t), 0., 1., 1.);
+
   float offset = step(mod(t, 4.), 2.);
   // float offset = 0.;
   colorInv *= r2d(e*PI + offset*PI);
   
-  // p *= r2d(e*PI + offset);
-  // p.y += sin(PI/2. + t*PI);
-  // p *= r2d(-t*10.);
+  p *= r2d(e*PI + offset*PI);//rotate view
+  // p.y = (e + offset) *2. -1.;
+  p.y += 1. + (modt * sin(PI/2. + t*PI/2.));// move up/down
+  // p *= r2d(-t*10.);//spin in-place
 
   float sz = .15;
   float circ = sdCircle(skew(p, .7), 0.25);
   float tri = sdTri(p-vec2(0.,sz/2.), sz, 0.5);
-  i = step(mix(tri, circ, remap(sin(t*2.5))), 0.);
+  i = step(mix(tri, circ, (0.) ), 0.);
 
   if(colorInv.y < 0.){i = 1. - i;}
   gl_FragColor = vec4(vec3(i, i, i),1.);
