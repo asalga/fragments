494e842d1c3e5acb93c8de90052635eace1dab60
Andor Saga <andor.saga@gmail.com>
2018-06-06 14:21:44 -0400

    63

--- src/chapters/fragments/code/0xff/60/63.fs
+++ src/chapters/fragments/code/0xff/60/63.fs
@@ -1,4 +1,4 @@
-// 63 - 
+// 63
 precision mediump float;
 uniform vec2 u_res;
 uniform float u_time;
@@ -10,18 +10,14 @@ float sdTri(vec2 p, float s, float sc){
   float u = p.y * sc;
   return max(distToSide + u, -u) - s;
 }
+//gsgd.co.uk/sandbox/jquery/easing/jquery.easing.1.3.js
 float easeInOutBack(float t, float b, float c, float d){
-  float s = 1.70158;
+  float s = .0;//70158;
   if ((t /= d/2.) < 1.){
   	return c/2.*(t*t*(((s*=(1.525))+1.)*t - s)) + b;
   }
   return c/2.*((t-=2.)*t*(((s*=(1.525))+1.)*t + s) + 2.) + b;
 }
-//gsgd.co.uk/sandbox/jquery/easing/jquery.easing.1.3.js
-float circ(float t, float b, float c, float d) {
-  return -c * (sqrt(1. - (t /= d)*t) - 1.) + b;
-}
-
 float sdCircle(vec2 p, float r){
   return length(p)-r;
 }
@@ -43,28 +39,22 @@ void main(){
   vec2 a = vec2(1., u_res.y/u_res.x);
   vec2 p = a * (gl_FragCoord.xy/u_res*2.-1.);
   float i;
+  float sz = .15;
   float t = u_time * 3.;
   vec2 colorInv = p;
-
-  // rotate only every second
   float modt = step(1., mod(t, 2.));
-  // float e = easeInOutBack( modt * fract(t), 0., .05, .5);
-  float e = circ( modt * fract(t), 0., 1., 1.);
-
+  float e = easeInOutBack( modt * fract(t), 0., 1., 1.);
   float offset = step(mod(t, 4.), 2.);
-  // float offset = 0.;
   colorInv *= r2d(e*PI + offset*PI);
-  
   p *= r2d(e*PI + offset*PI);//rotate 'camera'
-  p.y += sin(t*PI/2.);
-  float blendShape = (sin(t*PI/2.)+1.)/2.;
-  // p.y += 1. + (modt * sin(PI/2. + t*PI/2.));// move up/down
-  p *= r2d(t*5.);//spin in-place
+  e = easeInOutBack( modt * fract(t), 0., 1., 1.);
+  p.y += .6*sin(t*PI/2.);// move up/down
+  p *= r2d(t*8.);//spin in-place
 
-  float sz = .15;
   float circ = sdCircle(skew(p, .7), 0.25);
   float tri = sdTri(p-vec2(0.,sz/2.), sz, 0.5);
-  i = step(mix(tri, circ, blendShape), 0.);
+  float blendShape = (sin(t*PI/2.)+1.)/2.;
+  i = smoothstep(0.01, 0.001, mix(tri, circ, blendShape));
 
   if(colorInv.y < 0.){i = 1. - i;}
   gl_FragColor = vec4(vec3(i, i, i),1.);
