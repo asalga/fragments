ebe0f3ea6762cf733d6dcf273056670758685f0d
Andor Saga <andor.saga@gmail.com>
2018-06-07 12:28:21 -0400

    \.

--- src/chapters/fragments/code/0xff/60/64.fs
+++ src/chapters/fragments/code/0xff/60/64.fs
@@ -5,12 +5,10 @@ uniform float u_time;
 #define PI 3.141592658
 #define TAU 2.*PI
 #define NUM_STRIPS 10.
-
+#define NUM_VERT_STRIPS 4.
 vec2 swap(vec2 p){
   return vec2(p.y,p.x);
 }
-
-
 void main(){
   vec2 a = vec2(1., u_res.y/u_res.x);
   vec2 p = a * (gl_FragCoord.xy/u_res*2.-1.);
@@ -33,29 +31,37 @@ void main(){
   // Remove the Y then normalize / Project onto XZ-plane
   // The result is a normal on the XZ-plane
   vec3 xzVec = normalize(vec3(n.x, 0., n.z));
-  float thetaY = atan(xzVec.x, xzVec.z)+ u_time;
+  float thetaY = atan(xzVec.x,xzVec.z);// -PI/2 to PI/2
+  thetaY = (thetaY*2.)/PI;// -1 .. 1
+  thetaY = (thetaY+1.)/2.;// 0 .. 1
+  // thetaY += u_time/0.;// animate
 
-  float div = 1.;
-  float pLen = length(p);
+  // float i = step(mod(thetaY, 1./NUM_VERT_STRIPS), 0.24/2.);
+  //mod(thetaY, 1./NUM_VERT_STRIPS);
+  //step(0.025, fract(thetaY));
+  //step(div/4., mod(thetaY,div/2.));
 
-  // ew i know i know
-  // float horizSlices = 1.;
-  // float h = .25 * step(mod(p.y, horizSlices), horizSlices/2.);
-  // float i = step(div/4., mod(thetaY + h,div/2.)) * step(length(p), 1.);
+  // mod 0..1 into how many stripes we want
+  float m = mod(thetaY, 1./NUM_VERT_STRIPS);
 
-  // vertical stripes
-
-  // why div 4?
-  float i = step(div/4., mod(thetaY,div/2.));
+  // .5 since there are 2 states
+  float i = step(m * NUM_VERT_STRIPS, .5);
 
   gl_FragColor = vec4(vec3(i),1.);
-  // if(pLen > 1.){gl_FragColor = vec4(0,0,0,1);}
 }
+
 
 /*
 More notes:
 
 We 'could' get away without normalizing...But that also means 
 the pixels 'beyond' the sphere will get colored incorrectly.
+
+
+  // ew i know i know
+  // float horizSlices = 1.;
+  // float pLen = length(p);
+  // float h = .25 * step(mod(p.y, horizSlices), horizSlices/2.);
+  // float i = step(div/4., mod(thetaY + h,div/2.)) * step(length(p), 1.);
 
 */
