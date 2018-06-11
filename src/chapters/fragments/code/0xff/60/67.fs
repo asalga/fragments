321e46ba5233b721af753d6888e5b30c9df2b1b5
Andor Saga <andor.saga@gmail.com>
2018-06-11 10:08:48 -0400

    68

--- src/chapters/fragments/code/0xff/60/67.fs
+++ src/chapters/fragments/code/0xff/60/67.fs
@@ -7,7 +7,7 @@ float sdCircle(vec2 p, float r){
   return length(p)-r;
 }
 float circle(vec2 p, float r){
-  return step(sdCircle(p, r), 0.);
+  return smoothstep(0.01, 0.001, sdCircle(p, r));
 }
 
 void main(){
@@ -15,31 +15,24 @@ void main(){
   vec2 p = ar * (gl_FragCoord.xy/u_res*2.-1.);
   float NumSpaces = 8.;
   vec2 origP = p;
-  float r = .25;
-  float time = u_time*.5;
+  float r = .4;
+  float time = u_time*2.;
 
-  // float idx = (((p.x+1.)/2.)/NumSpaces)*10.;
-  float yIdx = (((p.y+1.)/2.)/2.)*10.;
+  float idx = (((p.x+1.)/2.)/NumSpaces)*10.;
 
-  float dir = step(p.y,0.)*2.-1.;
+  float dir =step(p.y,0.)*2.-1.;
   p.y = mod(p.y, 1.);
 
-  // p = mod(p, vec2(2./NumSpaces, 1.));
-  
-  
-  // center Y
-  p.y -= .5;
-  
-  // p.x -= r/2.;
-  p.x -= mod(time*dir,2.)-1.;
-  
-  // offset X to create staggerig pattern
-  // p.x += idx/1.;
+  p = mod(p, vec2(2./NumSpaces, 1.));
+  p.y -= .5;  // center Y
+  p.x -= idx/2.;
 
-  float i = circle(p,r);
+  vec2 p0 = p;
+  // move across canvas and account for radius
+  p.x  -= mod(time*dir,2.+r*2.)-1.-r;
+  p0.x -= mod((time+dir)*dir,2.+r*2.)-1.-r;
 
-  // if(gl_FragCoord.y/u_res.y > 0.5){
-  //   i = 1. - i;
-  // }
+  float i =  circle(p,r) + circle(p0,r);
+  if(gl_FragCoord.y/u_res.y > 0.5){i = 1. - i;}
   gl_FragColor = vec4(vec3(i),1);
 }
