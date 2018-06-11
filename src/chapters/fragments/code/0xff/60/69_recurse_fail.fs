071eed1952c36cdf7274bb512690bd8df764500d
Andor Saga <andor.saga@gmail.com>
2018-06-11 19:23:55 -0400

    70

--- src/chapters/fragments/code/0xff/60/69_recurse_fail.fs
+++ src/chapters/fragments/code/0xff/60/69_recurse_fail.fs
@@ -19,22 +19,22 @@ float ring(vec2 p, float r, float w){
 void main(){
   vec2 a = vec2(1., u_res.y/u_res.x);
   vec2 p = a * (gl_FragCoord.xy/u_res*2.-1.);
-  float time = mod(u_time*2., 1.);
+  float time = mod(u_time*1., 2.);
   float i;
   float strokeWeight = 10.;
   float r = 1.;
   float scale = 1.+time;
-  
+  p.y += 0.5;
   p /= scale;
   
-  vec2 v0 = vec2(0., time);
+  vec2 v0 = vec2(0.,  time);
   vec2 v1 = vec2(0., -1. + time);
   vec2 v2 = vec2(0., -2. + time);
 
-  strokeWeight = (1./u_res.x) * strokeWeight / scale;
+  strokeWeight = (1./u_res.x / p.y) * strokeWeight / scale;
 
   i += ring(p+v0,  1.*r, strokeWeight);
-  i += ring(p+v1, .5*r, strokeWeight);
+  i += ring(p+v1, .85*r, strokeWeight);
   // i += ring(p+v2, .25*r, strokeWeight);
 
   gl_FragColor = vec4(vec3(i),1.);
