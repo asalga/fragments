6c4b75b6b460798b16d884b43fe6d7847c659c40
Andor Saga <andor.saga@gmail.com>
2018-06-11 18:56:35 -0400

    \.

--- src/chapters/fragments/code/0xff/60/69_recurse.fs
+++ src/chapters/fragments/code/0xff/60/69_recurse.fs
@@ -19,44 +19,23 @@ float ring(vec2 p, float r, float w){
 void main(){
   vec2 a = vec2(1., u_res.y/u_res.x);
   vec2 p = a * (gl_FragCoord.xy/u_res*2.-1.);
-  float time = mod(u_time*.5, 1.);
+  float time = mod(u_time*2., 1.);
   float i;
-  float t = time;
-  float sw = 0.001;
+  float strokeWeight = 10.;
   float r = 1.;
-  float pos = .6;
-  // float TAN_ = tan(.647);
-  float theta = atan(pos/1.);
-
-  // p.y += time/1.25;
-  // r += time;
+  float scale = 1.+time;
   
-
-  float test = tan(theta) * t;
-  vec2 v = vec2(0., -pos + test );
-  vec2 v1 = vec2(0., pos + test );
+  p /= scale;
   
-  vec2 p1 = p;
-  vec2 p2 = p;
+  vec2 v0 = vec2(0., time);
+  vec2 v1 = vec2(0., -1. + time);
+  vec2 v2 = vec2(0., -2. + time);
 
-  p1 += v;
-  p1 /= 1. + t*9.;
+  strokeWeight = (1./u_res.x) * strokeWeight / scale;
 
-  p2 -= v1;
-  p2 /= 1. + t*9.;
+  i += ring(p+v0,  1.*r, strokeWeight);
+  i += ring(p+v1, .5*r, strokeWeight);
+  // i += ring(p+v2, .25*r, strokeWeight);
 
-  
-  i += ring(p, .8*r, sw);
-  i += ring(p1, .2*r, sw);
-  // i += ring(p2, .2*r, sw);
-
-  // i += ring(p2, .2*r, sw);
-  // i += ring(-p+v, 1.*r, sw);//lower
-
-  // vec2 v2 = vec2(0., -1.5 + t/2.);
   gl_FragColor = vec4(vec3(i),1.);
 }
-
-  // i += ring(p+v2, 1.5*r, sw);
-  // i += ring(p+vec2(0., .8)*r, .25*r, sw);
-  // i += ring(p+vec2(0., -.8)*r, .25*r, 0.01);
