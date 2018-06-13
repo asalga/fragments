8ed898e578ff633b7c0c978533093ca4d84aeb4e
Andor Saga <andor.saga@gmail.com>
2018-06-13 08:38:14 -0400

    70

--- src/chapters/fragments/code/0xff/70/70_.fs
+++ src/chapters/fragments/code/0xff/70/70_.fs
@@ -7,7 +7,7 @@ uniform float u_time;
 #define DEG_TO_RAD(x) (PI*(x/180.))
 
 #define EPSILON 0.001
-#define MAX_STEPS 16
+#define MAX_STEPS 255
 #define MAX_DIST 100.
 #define MIN_DIST 0.0
 
@@ -38,20 +38,40 @@ float rayMarch(vec3 eye, vec3 ray){
   return MAX_DIST;
 }
 
-vec3 createRay(float fov, vec2 dim, vec2 p) {
-  vec2 xy = p - (dim / 2.0);
-  float z = p.y / tan(fov/ 2.0);
-  return normalize(vec3(xy, -z));
-}
+// 300/tan( ((45/2)/180) * pi)
+vec3 createViewRay(float fov, vec2 sz, vec2 coord) {
+  // We want to specify FOV naturlaly as the angle 
+  // created from the eye to the sides of the cvs.
+  // But to work with the ratios, we need to divide by
+  // 2 to get a right triangle.
+  float halfFov = fov/2.;
 
+  // coord (0,0) .. (300,300)
+  // (-150,-150) .. 
+  vec2 xy = coord - (sz/2.);
+
+  // the greater then dimension of y, the more
+  // is assigned to the z ()
+  // Why divide the result of tan(45/2) 
+  float z = sz.y / tan(halfFov);
+
+  // far left
+  // -150, 0, -724  => (.2, 0. -.97)
+
+  // return vec3(0., 0., -1.);
+  // sz.y / .414;  // tan(halfFov);
+  return normalize(vec3(xy, -z));
+
+}
 
 void main(){
   vec2 a = vec2(1., u_res.y/u_res.x);
   vec2 p = a * (gl_FragCoord.xy/u_res*2.-1.);
   float i = 1.;
 
-  vec3 ray = createRay(DEG_TO_RAD(45.), vec2(400.), gl_FragCoord.xy);
-  vec3 eye = vec3(0., 0., 10.);
+  // increase FOV and sphere seems smaller
+  vec3 ray = createViewRay(DEG_TO_RAD(45.), u_res, gl_FragCoord.xy);
+  vec3 eye = vec3(0., 0., 5.);// why does 5 unit fill the entire canvas?
 
   float dist = rayMarch(eye, ray); 
 
