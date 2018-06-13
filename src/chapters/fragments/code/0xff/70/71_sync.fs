4cb1cb8da3dfd5678929e4443a78cd658f3bfbe1
Andor Saga <andor.saga@gmail.com>
2018-06-13 15:40:06 -0400

    71

---
+++ src/chapters/fragments/code/0xff/70/71_sync.fs
@@ -0,0 +1,12 @@
+precision mediump float;
+
+uniform vec2 u_res;
+uniform float u_time;
+
+void main(){
+  vec2 a = vec2(1., u_res.y/u_res.x);
+  vec2 p = a * (gl_FragCoord.xy/u_res*2.-1.);
+  float i;
+
+  gl_FragColor = vec4(vec3(i),1.);
+}
