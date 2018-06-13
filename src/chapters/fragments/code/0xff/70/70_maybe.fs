57f67da0bc834f759b8bb22184ac2ab9073ebf97
Andor Saga <andor.saga@gmail.com>
2018-06-13 13:18:35 -0400

    \.

--- src/chapters/fragments/code/0xff/70/70_maybe.fs
+++ src/chapters/fragments/code/0xff/70/70_maybe.fs
@@ -1,9 +1,12 @@
 // 70 - "Maybe"
 precision mediump float;
 #define PI 3.141592658
+#define TAU 2.*PI
+#define NUM_BTNS 10
 
 uniform vec2 u_res;
 uniform float u_time;
+
 
 float sdCircle(vec2 p, float r){
 	return length(p)-r;
@@ -27,12 +30,16 @@ float radio(vec2 p, float rad, float state){
 	p.x += rad; // center button
 
 	float button;
-	button = step(capsule(p, vec2(0.), vec2(rad*2., 0.), rad), 0.);
-	button -= step(capsule(p, vec2(0.), vec2(rad*2., 0.), rad-0.03), 0.);
+	button = smoothstep(0.01, 0.0001, capsule(p, vec2(0.), vec2(rad*2., 0.), rad));
+	button -= smoothstep(0.01, 0.0001, capsule(p, vec2(0.), vec2(rad*2., 0.), rad-0.03));
 	
 	p.x -= state*rad*2.;
 	button += circle(p, rad-0.01); // subtract a tiny bit, otherwise the circle  'peeks' out of the outline.
 	button = clamp(button, 0., 1.);
+
+	if(state == 1.){
+		button -= circle(p, rad-0.04);
+	}
 	return button;
 }
 
@@ -43,37 +50,30 @@ mat2 r2d(float a){
 void main(){
   vec2 a = vec2(1., u_res.y/u_res.x);
   vec2 p = a * (gl_FragCoord.xy/u_res*2.-1.);
-  float t = u_time * 4.00025;
+  float t = u_time * 2.;
   float fractTime = fract(t);
   float i;
   float r = 0.25;
-  float startX = .25;
+  float isLightOn;
 
-  float t2 = t + 1.;
+  for(int it = 0; it < NUM_BTNS; ++it){
+  	vec2 _p = p* 3.;// scale down
 
-  float buttonPos1 = min(mod(t, 2.), 1.);
-  float buttonPos2 = min(mod(t2, 2.), 1.);
+  	float an = float(it)/float(NUM_BTNS) * TAU;
+  	_p *= r2d(an);
+  	_p += vec2(1.5, 0.);
+  	
+  	float _t = t + an/20.;
+  	float buttonPos = min(mod(_t, 2.), 1.);
+		float buttonState = step(1.,mod(_t,4.)) * step(mod(_t,4.), 3.);
+ 		mat2 rot1 = r2d(PI*step(2., mod(_t,4.)));
 
-  //  _|``|_
-  float buttonState1 = step(1.,mod(t,4.)) * step(mod(t,4.), 3.);
-  float buttonState2 = step(1.,mod(t2,4.)) * step(mod(t2,4.), 3.);
- 
-  mat2 rot1 = r2d(PI*step(2., mod(t,4.)));
-  mat2 rot2 = r2d(PI*step(2., mod(t2,4.)));
- 
- 
-
-  i += radio(p*rot1, r, buttonPos1);
-  i += radio((p+ vec2(0., 0.5))*rot2, r, buttonPos2);
- 
-
-
-
-  float inv = buttonState1 + buttonState2;
-  // i = abs(inv - i);
-  if(mod(inv, 2.)== 0.){
-  	i = 1. - i;
+  	i += radio(_p*rot1, r, buttonPos);
+  	isLightOn += buttonState;
   }
 
+  if(mod(isLightOn, 2.)== 0.){
+  	// i = 1. - i;
+  }
   gl_FragColor = vec4(vec3(i,i,i),1.);
 }
