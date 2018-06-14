eb7ebc96d6d5fc1fcbea738700d1f8945081d4d5
Andor Saga <andor.saga@gmail.com>
2018-06-14 11:49:08 -0400

    71

--- src/chapters/fragments/code/0xff/70/71_contours.fs
+++ src/chapters/fragments/code/0xff/70/71_contours.fs
@@ -2,13 +2,19 @@ precision mediump float;
 uniform vec2 u_res;
 uniform float u_time;
 #define PI 3.141592658
+float circle(vec2 p, float r){
+  return smoothstep(0.01, 0.001, length(p)-r);
+}
 void main(){
   vec2 a = vec2(1.,u_res.y/u_res.x);
-  vec2 p = 1.1*a*(gl_FragCoord.xy / u_res * 2. -1.);
+  vec2 p = a*(gl_FragCoord.xy / u_res * 2. -1.);
   float t = u_time;
-  vec3 l = vec3(cos(t), 0., sin(t));
+
+  p += 0.4;
+  vec3 l = normalize(vec3(.7, .7, 0.9));
   float z = sqrt(1. - p.x*p.x - p.y*p.y);
   vec3 v = normalize(vec3(p.x, p.y, z));
-  float c = step(sin(u_time*50.+ dot(v,l)*50.), 0.);
+  float c = smoothstep(0.1, 0.0001, sin(u_time*10.*PI + dot(v,l)*30.));
+  // c += circle(p-=1.5, 0.7);
   gl_FragColor = vec4(vec3(c), 1.);
 }
