2a663e673218da308be42ff3e1378533f2a845af
Andor Saga <andor.saga@gmail.com>
2018-06-14 11:25:03 -0400

    71

--- src/chapters/fragments/code/0xff/70/71_contours.fs
+++ src/chapters/fragments/code/0xff/70/71_contours.fs
@@ -2,9 +2,6 @@ precision mediump float;
 uniform vec2 u_res;
 uniform float u_time;
 #define PI 3.141592658
-float random (vec2 st) {
-  return fract(sin(dot(st.xy,vec2(12.9898,178.233)))*43758.5453123);
-}
 void main(){
   vec2 a = vec2(1.,u_res.y/u_res.x);
   vec2 p = 1.1*a*(gl_FragCoord.xy / u_res * 2. -1.);
