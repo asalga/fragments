cb59b396eb4512b7b880efc117297d441b2a35be
Andor Saga <andor.saga@gmail.com>
2018-06-16 14:49:04 -0400

    73

--- src/chapters/fragments/code/0xff/70/73_.fs
+++ src/chapters/fragments/code/0xff/70/73_.fs
@@ -8,34 +8,47 @@ float sdSquare(vec2 p, float halfW){
   return max(_p.x,_p.y)-halfW;
 }
 float square(vec2 p, float halfW){
-	return step(sdSquare(p, halfW), 0.);
+  return smoothstep(0.01, 0.001, sdSquare(p, halfW));
 }
 float sdCircle(vec2 p, float r){
   return length(p)-r;
 }
 float circle(vec2 p, float r){
-	return step(sdCircle(p,r), 0.);
+  return smoothstep(0.01, 0.001, sdCircle(p,r));
 }
 mat2 r2d(float a){
-	return mat2(cos(a),-sin(a),sin(a),cos(a));
+  return mat2(cos(a),-sin(a),sin(a),cos(a));
 }
 void main(){
+  const int cnt = 16;
+  float time = u_time*2.;
   vec2 a = vec2(1., u_res.y/u_res.x);
   vec2 p = a * (gl_FragCoord.xy/u_res*2.-1.);
+  mat2 rot = r2d(time* 2.0);
+  float sz = 0.45;
+  float rad = 0.06;
   float i;
-  float time = u_time;
- 	mat2 r = r2d(time*.1);
 
-  // vec2 squarePos = p + vec2(0., sin(time*0.));//* r2d(time*1.);
-  vec2 squarePos = p * r;
- 
-	vec2 v = vec2(0.25, -0.25);
-  vec2 c0 = p + v;
+  for(int it=0;it<cnt;++it){
+    float _it = float(it);
+    float offset = (mod(_it,4.)/3.) *2. -1.;
+    vec2 v, shift, nudge;
 
-  float dist = sdSquare( (v*r), 0.25);
-  i += circle(c0 + vec2(0., -dist*2.), 0.02);
+    if(it < 4){
+       v = vec2(offset*sz, -sz);
+       shift = vec2(0., 1.);
+       nudge = vec2(0., -.2);
+    }
+    else if(it > 7 && it < 12){
+       v = vec2(offset*sz, sz);
+       shift = vec2(0., -1.);
+       nudge = vec2(0., .2);
+    } 
+    float dist = sdSquare(v*rot, sz);
+    shift *= dist*1.4;// + pow(1.1, dist);
+    i += circle(p + v + shift + nudge, rad);
+  }
 
-  i += square(squarePos, 0.25);
-
+  i += square(p * rot, sz);
   gl_FragColor = vec4(vec3(i),1.);
 }
