a0b04e697849d0f0a6f21b09d993d922654fb591
Andor Saga <andor.saga@gmail.com>
2018-06-16 19:01:32 -0400

    73

--- src/chapters/fragments/code/0xff/70/73_.fs
+++ src/chapters/fragments/code/0xff/70/73_.fs
@@ -20,34 +20,55 @@ mat2 r2d(float a){
   return mat2(cos(a),-sin(a),sin(a),cos(a));
 }
 void main(){
-  const int cnt = 16;
-  float time = u_time*2.;
+  const float cnt = 10.;
+  float time = u_time*4.;
+  float t = time * 0.1;
+
   vec2 a = vec2(1., u_res.y/u_res.x);
   vec2 p = a * (gl_FragCoord.xy/u_res*2.-1.);
-  mat2 rot = r2d(time* 2.0);
+  mat2 rot = r2d(-time);
   float sz = 0.45;
   float rad = 0.06;
   float i;
 
-  for(int it=0;it<cnt;++it){
-    float _it = float(it);
-    float offset = (mod(_it,4.)/3.) *2. -1.;
-    vec2 v, shift, nudge;
+  // hack so I don't have to deal with hiding
+  // circle that appear on far left
+  p *= .9;
 
-    if(it < 4){
-       v = vec2(offset*sz, -sz);
-       shift = vec2(0., 1.);
-       nudge = vec2(0., -.2);
+  
+    for(float it=0.;it<10.;++it){
+      vec2 v, shift, nudge;
+      float offset;
+      float pct = 0.2;
+
+      offset = ((it/(cnt-1.))*2.-1.)*2. + pct;
+      float mv = mod(t, pct);
+      
+      v = vec2(offset * sz - mv, -sz);
+      nudge = vec2(0., -.2);
+       
+      float dist = sdSquare(v*rot, sz);
+      shift = vec2(0., clamp(dist*1.5, -.4, .15));
+      i += circle(p + v + shift + nudge, rad);
     }
-    else if(it > 7 && it < 12){
-       v = vec2(offset*sz, sz);
-       shift = vec2(0., -1.);
-       nudge = vec2(0., .2);
-    } 
-    float dist = sdSquare(v*rot, sz);
-    shift *= dist*1.4;// + pow(1.1, dist);
-    i += circle(p + v + shift + nudge, rad);
-  }
+  
+    // too sleepy to fix this right now.
+    // bottom
+    for(float it=0.;it<10.;++it){
+      vec2 v, shift, nudge;
+      float offset;
+      float pct = 0.2;
+
+      offset = ((it/(cnt-1.))*2.-1.)*2. + pct;
+      float mv = mod(-t, pct);
+      
+      v = vec2(offset * sz - mv, sz);
+      nudge = vec2(0., -.25);
+       
+      float dist = sdSquare(v*rot, sz);
+      shift = vec2(0., clamp(dist*1.5, -.4, .15));
+      i += circle(p + v - shift + nudge + vec2(0., sz), rad);
+    }
 
   i += square(p * rot, sz);
   gl_FragColor = vec4(vec3(i),1.);
