a4162a946ef01c7ac9cdf22455475e6239a3b5e1
Andor Saga <andor.saga@gmail.com>
2018-06-17 13:03:25 -0400

    \.

--- src/chapters/fragments/code/0xff/70/74_spacetime.fs
+++ src/chapters/fragments/code/0xff/70/74_spacetime.fs
@@ -17,6 +17,13 @@ float every(float v, float i, float c, float stay){
 	return x;
 }
 
+
+// from iquilezles 
+float impulse(float k, float x){
+  float h = k*x;
+  return h*exp(1.0-h);
+}
+
 mat2 r2d(float a){
 	return mat2(cos(a),-sin(a),sin(a), cos(a));
 }
@@ -25,18 +32,49 @@ void main(){
   vec2 a = vec2(1., u_res.y/u_res.x);
   vec2 p = a * (gl_FragCoord.xy/u_res*2.-1.);
   float i;
-  float t = u_time*1.;
+  float t = u_time * 1.0;
+  const float cnt = 4.;
+
+  float mvX = every(t     , 1., 2., 1.);
+  float mvY = every(t + 1., 1., 2., 0.);
+
+  float flip = step(mod(t, 4.),2.)*2.-1.;
+  p *= flip;
+	float sz = .25;
+
   
-  float mvX = every(t   , 1., 2., 1.);
-  float mvY = every(t+1., 1., 2., 0.);
+  for(float it=0.;it < cnt;it++){
+  	float pct = (it/(cnt));
+  
+	  mvX = impulse(1., mvX);
+	  mvY = impulse(1., mvY);
 
-  vec2 pos = vec2(.5-mvX, -.5+mvY);
+	  vec2 pos = vec2(.5-mvX, -.5+mvY);
 
-  float flip = (step(mod(t, 4.),2.)*2.)-1.;
-  p.x *= flip;
-  p.y *= flip;
-
-  i = step(sdSquare(p+pos,0.25),0.);
+  
+  	float alpha = 0.25;
+  	i += step(sdSquare(p+pos, sz),0.) * alpha;
+	}
 
   gl_FragColor = vec4(vec3(i),1.);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
