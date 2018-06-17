e7601733dadb8feffd4f88ab7db28cdc35895c3d
Andor Saga <andor.saga@gmail.com>
2018-06-17 11:25:48 -0400

    74

--- src/chapters/fragments/code/0xff/70/74_spacetime.fs
+++ src/chapters/fragments/code/0xff/70/74_spacetime.fs
@@ -2,14 +2,23 @@ precision mediump float;
 
 uniform vec2 u_res;
 uniform float u_time;
+#define PI 3.141592658
 
 float sdSquare(vec2 p, float halfW){
   vec2 _p = abs(p);
   return max(_p.x,_p.y)-halfW;
 }
 
-float every(float v, float i, float c){
-	return mod(v, i) * step(mod(v, c), i);
+float every(float v, float i, float c, float stay){
+	float alternate = step(mod(v, c), i);
+	float x = mod(v, i) * alternate;
+	// we reached the 'end'
+	if(stay == 1. && alternate==0.){x = i;}
+	return x;
+}
+
+mat2 r2d(float a){
+	return mat2(cos(a),-sin(a),sin(a), cos(a));
 }
 
 void main(){
@@ -18,9 +27,14 @@ void main(){
   float i;
   float t = u_time*1.;
   
-  float tm = every(t, 1., 2.);
+  float mvX = every(t   , 1., 2., 1.);
+  float mvY = every(t+1., 1., 2., 0.);
 
-  vec2 pos = vec2(.5-tm,-.5);
+  vec2 pos = vec2(.5-mvX, -.5+mvY);
+
+  float flip = (step(mod(t, 4.),2.)*2.)-1.;
+  p.x *= flip;
+  p.y *= flip;
 
   i = step(sdSquare(p+pos,0.25),0.);
 
