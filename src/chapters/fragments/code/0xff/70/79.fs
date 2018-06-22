063d1979b1c4a59e1f62f08ccb6abac6b51653be
Andor Saga <andor.saga@gmail.com>
2018-06-21 20:54:28 -0400

    79

--- src/chapters/fragments/code/0xff/70/79.fs
+++ src/chapters/fragments/code/0xff/70/79.fs
@@ -1,31 +1,23 @@
-// 79 - 
 precision mediump float;
 
 uniform vec2 u_res;
 uniform float u_time;
 #define PI 3.141592658
-
-float circle(vec2 p, float r){
-  return smoothstep(0.01, 0.001, length(p)-r);
-}
+#define TAU (PI*2.)
 
 void main(){
-  vec2 a = vec2(1.,u_res.y/u_res.x);
-  vec2 p = a*(gl_FragCoord.xy / u_res * 2. -1.);
-  float t = u_time * 0.91;
+  vec2 a = vec2(1., u_res.y/u_res.x);
+  vec2 p = a * (gl_FragCoord.xy/u_res*2.-1.);
+  float i;
+  float t = u_time * .0;
+  float mv = p.x * 1. + t;
 
-  // p += 0.4;
+  float corner = p.x/10.;
+  float y = p.y + sin(corner * TAU + t)*0.5;
+  //-.5 + sin(t)*0.15;
 
-  vec3 l = normalize(vec3(.7, .7, 0.9));
-  float z = sqrt(1. - p.x*p.x - p.y*p.y);
-  
-  vec3 v1 = normalize(vec3(p.x, p.y, z));
-  vec3 v2 = normalize(vec3(p.x, p.y, -z));
+  i = step(fract(mv), 0.5) * step(y, -0.25);
+  	  // step(fract(mv), 0.5) * step(0.,y-0.25);
 
-  float c = smoothstep(0.1, 0.0001, sin(t*10. + dot(v1,l)*30.));
-  float c2 = smoothstep(0.1, 0.0001, sin(t*10. + dot(v2,l)*30.) )/2.;
-
-  c += c2;
-  // c += circle(p-=1.5, 0.7);
-  gl_FragColor = vec4(vec3(c), 1.);
+  gl_FragColor = vec4(vec3(i),1.);
 }
