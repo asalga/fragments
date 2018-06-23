d4b59525aa4376730655a12e7ff7fe6d23b18d35
Andor Saga <andor.saga@gmail.com>
2018-06-23 13:01:41 -0400

    82

--- src/chapters/fragments/code/0xff/80/82_sine_light.fs
+++ src/chapters/fragments/code/0xff/80/82_sine_light.fs
@@ -1,1 +1,22 @@
-// 82 - Sine Light
+// 82 - "Sine Light"
+precision mediump float;
+
+uniform vec2 u_res;
+uniform float u_time;
+#define PI 3.141592658
+
+void main(){
+  vec2 a = vec2(1., u_res.y/u_res.x);
+  vec2 p = a * (gl_FragCoord.xy/u_res*2.-1.);
+  float i;
+  vec3 light = vec3(0,0,2);
+
+  // 1 cylinder waves - DONE
+  i = (sin(p.x*PI*4.)+1.)/2.;
+
+
+
+  // 2 create normals
+
+  gl_FragColor = vec4(vec3(i),1.);
+}
