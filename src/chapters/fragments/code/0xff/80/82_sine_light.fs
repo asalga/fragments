0f9d5e404acba2ee1eb30d58e9bc80e8785c0c2e
Andor Saga <andor.saga@gmail.com>
2018-06-24 12:01:08 -0400

    82

--- src/chapters/fragments/code/0xff/80/82_sine_light.fs
+++ src/chapters/fragments/code/0xff/80/82_sine_light.fs
@@ -9,14 +9,14 @@ void main(){
   vec2 a = vec2(1., u_res.y/u_res.x);
   vec2 p = a * (gl_FragCoord.xy/u_res*2.-1.);
   float i;
-  vec3 light = vec3(0,0,2);
 
-  // 1 cylinder waves - DONE
+  vec3 lightPos = vec3(0,0,2);
   i = (sin(p.x*PI*4.)+1.)/2.;
 
-
+  float wavePos = sin(p.x*PI*4.);
 
   // 2 create normals
+  vec3 n = normalize(vec3(cos(wavePos),0,sin(wavePos)));
 
   gl_FragColor = vec4(vec3(i),1.);
 }
