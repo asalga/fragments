e8b9f0c0b57c7f4e3b2afad49125001c3ed967ec
Andor Saga <andor.saga@gmail.com>
2018-07-08 10:39:52 -0400

    98

--- src/chapters/fragments/code/ex/_1d_noise_1.fs
+++ src/chapters/fragments/code/ex/_1d_noise_1.fs
@@ -1,11 +1,12 @@
 precision mediump float;
 
 uniform vec2 u_res;
+uniform float u_time;
 
 void main(){
 	vec2 p = (gl_FragCoord.xy/u_res) * 2. -1.;
 
-	vec2 i = fract(sin(p.x* 12342. + p.y * 3433.)*234343.);
+	float i = fract(sin(p.x* 12342. + p.y * 3433. + u_time)*234343.);
 
 	gl_FragColor = vec4(vec3(i), 1);
 }
