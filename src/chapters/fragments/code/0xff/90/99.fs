542888e2c0b7c4e6c8c5ab03c7c5a99d6a310454
Andor Saga <andor.saga@gmail.com>
2018-07-12 16:24:25 -0400

    .

--- src/chapters/fragments/code/0xff/90/99.fs
+++ src/chapters/fragments/code/0xff/90/99.fs
@@ -1,17 +1,18 @@
-// 90 - Terrain
+// 90 - Space Roads
 precision mediump float;
 
 uniform vec2 u_res;
 uniform float u_time;
 
-#define PI 3.141592658
+#define PI 3.14159265834
 #define TAU (PI*2.)
-const float SPEED = 4.;
+const float SPEED = 1.1;
 
-const int MaxStep = 128;
+const int MaxStep = 200;
 const int MaxShadowStep = 100;
-const float MaxDist = 400.;
-const float Epsilon = 0.0001;
+const float MaxDist = 50.;
+const float MinDist = -0.;
+const float Epsilon = 0.001;
 
 const vec3 lightGrey = vec3(1.);
 const vec3 darkGrey = vec3(0.4);
@@ -64,37 +65,37 @@ float sdSphere(vec3 p, float r){
 float sdScene(in vec3 p, out vec3 col){
 	float sz = 1.;
 	vec3 offset = vec3(0, 0.5, 0);
+	float t = u_time * SPEED;
 
-	vec3 np = p + offset;
-	np.x = mod(p.x, sz) - sz * .5;
-	np.z = mod(p.z, sz) - sz * .5;
+	vec3 np = p;
+	np.x = mod(p.x, sz) -  .5*sz;
+	np.z = mod(p.z, sz) -  .5*sz;
 
 	// float n = valueNoise(vec2(0., step(.5, mod(np.z, 1.))  ) );
-	vec3 cell = floor( p/1. ) * 10.;
-	// cell += vec3(0.0625, 0, 0.125);
-	float n = valueNoise(cell.zx / 10.);
+	vec3 cell = floor(p) * 2.;
+	float n = valueNoise(cell.zx);
+	// n = step(0.5, n);
 
-	// n = step(.15, n);
-	// n *= 1.;
-	// n = floor(n*10.)/10.;
-	n = step(0.5, n);
+	float holes = cubeSDF(np + vec3(0, 0, 0),  vec3(.5001, .2*n, .5001));
 
-	float holes = cubeSDF(np + vec3(0, .5 - n/10., 0),  vec3(.5001, n*1., .5001));
+	// np = p;
 
-	float t = u_time * SPEED;
-	np = p;// + vec3(0, .5, -t);
-	float road = cubeSDF(np, vec3(1, .025, 100));
+	float x = step(mod(p.x, sz), sz * .5 );
+	float y = step(mod(p.z, sz), sz * .5 );
 
-	float x = step(mod(np.x, sz), sz * .5 );
-	float y = step(mod(np.z, sz), sz * .5 );
+	float road = cubeSDF(np - vec3(0,  sign(sin( (p.z/1.) * PI )) * n * 0.01  , 0), vec3(.5, .01, .5));
 
-	// Road checkerboard
-	// TOOD: fix
+	// Road checkerboard TODO: fix
 	float colId = xor(x,y);
 	col = darkGrey;
 	if(colId == 0.) col = lightGrey;
 
-	// return road;
+	if(n == 0.){
+		// col *= valueNoise(cell.zx) * np.x;
+	}
+	
+
+	return road;
 	// return n;
 	// return min(road, holes);
 	// return holes;
@@ -111,16 +112,16 @@ vec3 estimateNormal(vec3 v){
 }
 
 float rayMarch(vec3 ro, vec3 rd, out vec3 col){
-  float s = 0.;
+  float s = MinDist;
   for(int i = 0; i < MaxStep; ++i){
     vec3 v = ro + (rd*s);
     
     float dist = sdScene(v, col);
 
     if(dist < Epsilon){
-      return s;
+      return -1.;
     }
-    s += dist;
+    s += dist/4.;
 
     if(s >= MaxDist){
       return MaxDist;
@@ -171,23 +172,24 @@ void main(){
   vec2 p = (gl_FragCoord.xy/u_res)*2.-1.;
   vec3 i;
   float t = u_time * SPEED;
-  vec3 eye = vec3(0., 1., 5. - t);
-  vec3 ray = rayDirection(85.0, u_res, gl_FragCoord.xy);
-  vec3 lightPos = vec3(0, 5, 5. - t);
+  vec3 eye = vec3( 0., 1.0, 2. - t*2.);
+  vec3 ray = rayDirection(140.0, u_res, gl_FragCoord.xy);
+  vec3 lightPos = eye +
+  vec3(0, 5, 1);
 
   vec3 col;
   float d = rayMarch(eye, ray, col);
-  vec3 point = eye+ray*(d);
-  
-  vec3 n = estimateNormal(point);
+ 
 
   if(d < MaxDist){
-		i += col * phong(point, n, lightPos);
+  	// vec3 point = eye+ray*(d);
+  	// vec3 n = estimateNormal(point);
+		i += col;// * phong(point, n, lightPos);
 		// i += phong(point, n, lightPos);
 	}
 
-	float fog = 1./pow(d, .02);
-	// i *= fog;
+	float fog = 1./pow(d, .15);
+	i *= fog;
 
   gl_FragColor = vec4(vec3(i),1);
 }
