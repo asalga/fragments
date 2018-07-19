fecd37282db097e5223ef8dc1e74fac83418292c
Andor Saga <andor.saga@gmail.com>
2018-07-19 08:54:51 -0400

    106

--- src/chapters/fragments/code/0xff/100_199/0/106.fs
+++ src/chapters/fragments/code/0xff/100_199/0/106.fs
@@ -1,35 +1,27 @@
-// 106 - 
+// 106 - Moiré 
 precision mediump float;
 uniform vec2 u_res;
 uniform float u_time;
 
 float sdCircle(vec2 p, float r){
-	return sin(length(p)*20.) - r;
-}
-
-float sdRect(vec2 p, vec2 size){
-  vec2 d = abs( p) - size;
-  return min(max(d.x,d.y), .0) + length(max(d,.0));
+	return sin(length(p)*50.) - r;
 }
 
 void main(){
 	float i;
-	float t = u_time*2.;
 	vec2 p = (gl_FragCoord.xy/u_res)*2.-1.;
+	float t = u_time*3.;
 
-	vec2 m = step(mod(p, vec2(0.1)), vec2(0.1));
-
-	vec2 tr = vec2(sin(t), 0.);
-	float c0 = step(sdCircle(p + tr, .1), 0.);
-	float c1 = step(sdCircle(p - tr, .1), 0.);
+	float d = .5;
+	vec2 tr = vec2(cos(t) * d, 0.);
+	float c0 = step(sdCircle(p, .021), 0.);
+	float c1 = step(sdCircle(p + tr, .021), 0.);
 	i = c0 + c1;
 
 	float r = c0+c1;
-	if(r == 2. && fract(t) < 0.5 ){
+	if(r == 2. && tr.x < 0.){
 		i = 0.;
 	}
-
-	// i = step(0., sdRect(m, vec2(.5)));
 
 	gl_FragColor = vec4(vec3(i),1);
 }
