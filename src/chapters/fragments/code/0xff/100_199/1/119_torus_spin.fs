97ed0a8ac8b31100561792c63150dddd2b558653
Andor Saga <andor.saga@gmail.com>
2018-07-31 18:36:27 -0400

    119

--- src/chapters/fragments/code/0xff/100_199/1/119_torus_spin.fs
+++ src/chapters/fragments/code/0xff/100_199/1/119_torus_spin.fs
@@ -10,34 +10,6 @@ const float PI = 3.141592658;
 const float TAU = PI*2.;
 const float HALF_PI = PI*0.5;
 
-const vec3 lightGrey = vec3(1.);
-const vec3 darkGrey = vec3(0.4);
-
-float sampleCheckerboard(vec2 c) {
-  vec2 sz = vec2(0.008);
-  vec2 co = ((mod(c,sz*2.)-sz*1.)+1.)/2.;
-
-  vec2 s = step(co, vec2(.5) );
-  return s.x;
-  //if(s.x == 1.){return 0.;}
-  //return 1.;
-}
-
-float rectSDF(vec2 p, vec2 size) {//book of shaders
-  vec2 d = abs(p) - size;
-  return min(max(d.x,d.y),.0)+length(max(d,.0));
-}
-
-float sample(vec2 c) {
-  vec2 sz = vec2(0.005* PI);
-  vec2 co = mod(c,sz)-sz*0.5;
-  return rectSDF(co, vec2(0.0125)) * 8.;
-}
-
-float sdEllipsoid( in vec3 p, in vec3 r ){
-    return (length( p/r ) - 1.0) * min(min(r.x,r.y),r.z);
-}
-
 // x => overall size/radius
 // y => thickness
 float sdTorus(vec3 p, vec2 t){  
@@ -60,11 +32,11 @@ mat3 viewMatrix(vec3 eye, vec3 center, vec3 up) {
   return mat3(s, u, -f);
 }
 
-float lighting(vec3 p, vec3 n, vec3 lightPos){
-  float ambient = 0.3;
+float lighting(vec3 p, vec3 n, vec3 lightPos, vec3 eye){
+  float ambient = 0.01;
   // ---
   vec3 pToLight = vec3(lightPos - p);
-  float power = 24.;
+  float power = 14.;
   vec3 lightRayDir = normalize(pToLight);
   float d = length(pToLight);
   d *= d;
@@ -72,19 +44,13 @@ float lighting(vec3 p, vec3 n, vec3 lightPos){
   float diffuse = (nDotL*power) / d;
   float kd = 1.;
 
-  // ---
-  float gloss = 100.;
-  vec3 H = normalize(lightRayDir + p);
-  float NdotH = max(dot(n, H), .0);
-  // vec3 r = reflect(lightRayDir, n);
-  // float RdotV = max( 0., dot(r, normalize(p)) );
-  float spec = pow( NdotH , gloss )/d;
-  // float spec = pow(RdotV, gloss);
-  float ks = 0.;
-  // ---
+  float gloss = 10.;    
+  vec3 V = normalize(eye - p);
+  vec3 R = normalize(reflect(-lightPos, n)); 
+  float dotRV = dot(R, V);
+  float spec = pow(dotRV, gloss);
 
-  return ambient + diffuse;// + spec;
-  // return spec;
+  return ambient + diffuse + spec;
 }
 
 vec3 rayDirection(float fieldOfView, vec2 size, vec2 fragCoord) {
@@ -102,9 +68,6 @@ mat4 r2dY(float a){
               s,  0, c,  0,
               0,  0, 0,  1);
 }
-
-
-
 mat4 r2dX(float a){
   float c = cos(a);
   float s = sin(a);
@@ -114,7 +77,6 @@ mat4 r2dX(float a){
               0,  s,  c,  0,
               0,  0,  0,  1);
 }
-
 mat4 r2dZ(float a){
   float c = cos(a);
   float s = sin(a);
@@ -124,56 +86,57 @@ mat4 r2dZ(float a){
               0,  0, 1,  0,
               0,  0, 0,  1);
 }
+mat4 scale(float x, float y, float z){
+  return mat4(x, 0, 0, 0,
+              0, y, 0, 0,
+              0, 0, z, 0,
+              0, 0, 0, 1);
+}
 
-float sdScene(vec3 p, out float col){
-  float t = u_time/1.;
+float sdScene(vec3 p, out vec3 rot){
+  float t = u_time * 1.0;
 
-  vec3 n = p;
-  n = normalize(n);
+  float _1 =       - t;
+  float _2 = PI/2. - t;
 
-  vec2 uv = vec2(atan(n.x, n.z)/PI + .5,
-                  asin(n.y)/PI + 0.5);
-  // if(n.x < 0.){
-  //   uv.y *- -1.;
-  // }
-  // uv.x += t * .25;
-  uv *= 0.25;
+  vec2 dims = vec2(1.4, 0.7);
+
+  vec3 rot_t1 = (vec4(p - vec3(dims.x/2. , 0,0),1) * r2dX(_1)).xyz;
+  vec3 rot_t2 = (vec4(p - vec3(-dims.x/2. ,0,0),1) * r2dX(_2)).xyz;
+
+  float t1 = sdTorus(rot_t1, dims);
+  float t2 = sdTorus(rot_t2, dims);
+
+  if(t1 < Epsilon){
+    rot = vec3(_1,0,0);
+  }
+  if(t2 < Epsilon){
+    rot = vec3(_2, 0,0);
+  }
  
-  // col = sampleCheckerboard(uv);
-
-  float t1 = sdTorus( (vec4(p,1) * r2dX(t*0.00)).xyz + vec3(1,0,0), vec2(1.5, 0.85));
-  // float t1 = sdTorus( p, vec2(1.5, 0.85));
-  // float t2 = sdTorus(p+vec3(1.75,0,0), vec2(1., 0.5));
-  // float t3 = sdTorus(p+vec3(-1.75,0,0), vec2(1., 0.5));
-  // float res = min(t1, t2);
-  // res = min(res, t3);
-
-  return t1;
-  // return res;
+  return min(t2,t1);
 }
 
 vec3 estimateNormal(vec3 v){
   vec3 n;
-  float dum;
+  vec3 dum;
   n.x = sdScene(vec3(v.x + Epsilon, v.y, v.z),dum) - sdScene( vec3(v.x - Epsilon, v.y, v.z),dum);
   n.y = sdScene(vec3(v.x, v.y + Epsilon, v.z),dum) - sdScene( vec3(v.x, v.y - Epsilon, v.z),dum);
   n.z = sdScene(vec3(v.x, v.y, v.z + Epsilon),dum) - sdScene( vec3(v.x, v.y, v.z - Epsilon),dum);
   return normalize(n);
 }
 
-float rayMarch(vec3 ro, vec3 rd, out vec3 col){
+float rayMarch(vec3 ro, vec3 rd, out vec3 rot){
   float s = 0.;
   for(int i = 0; i < MaxSteps; i++){
     vec3 p = ro + rd * s;
     
-    float intensity;
-    float d = sdScene(p, intensity);
-    col = vec3(intensity);
+    float d = sdScene(p, rot);
     
     if(d < Epsilon){
       return s;
     }
-    s += d/2.;
+    s += d;
 
     if(d > MaxDist){
       return MaxDist;
@@ -182,66 +145,46 @@ float rayMarch(vec3 ro, vec3 rd, out vec3 col){
   return MaxDist;
 }
 
-float getColor(vec3 n){
-  float t = u_time * 0.0;
-
-  // float u = acos(n.y/1.)*TAU;
-  // float v = acos(n.x/(1.+ 0.5 *cos(2.*PI) ))*TAU;
-  // vec2 uv = vec2(u,v);
+float getColor(vec3 n, vec3 p){
+  float t = u_time * 0.4;
+  float a = atan(n.x, n.z);
   
-  float a = atan(n.x, n.z)/PI + .5;
-  vec2 uv = vec2(a+t, 0.);
-
-
-  vec3 up = vec3(0,1,0);
-  float an = acos( dot(up,n));
-
-  if(an > PI/2.){
-    // uv.x = 0.;
-  }
-
-  // if(n.x < 0. && n.z < 0.){
-  //   uv.x *= 0.1;
-  //   // uv.x = 0.;
-  // }
-  
-
-  // uv.x += t * .25;
-  uv *= 0.25 / PI;
-
-  return sampleCheckerboard(uv);
+  float u = ((atan(n.z/n.x))*2.+PI)/ TAU;
+ 
+  float sz = 0.25;
+  return step(mod(u+t, sz), sz/2.) + 0.25;
 }
 
 void main(){
   float i;
-  float t = -u_time*.5;
-  vec2 fc = gl_FragCoord.xy;
+  // float t = u_time * 0.0;
 
   float dist = 4.;
   // vec3 eye = vec3(dist * cos(t), sin(t) * 3., dist * sin(t));
-  vec3 eye = vec3(0, 7, 4);
-  vec3 center = vec3(0, 0, 0.);
-  vec3 lightPos =  vec3(0., 0., 0) + eye;
+  vec3 eye = vec3(0, 10, 5);
+  vec3 center = vec3(0);
+  vec3 lightPos =  vec3(0,5,2) + eye;
   vec3 up = vec3(0,1,0);
 
-  mat3 viewWorld = viewMatrix(eye, center, up);
-  vec3 ray = rayDirection(70., u_res, fc);
+  vec3 ray = rayDirection(70., u_res, gl_FragCoord.xy);
 
-  vec3 worldDir = viewWorld * ray;
-  vec3 col;
-  float d = rayMarch(eye, worldDir, col);
-  vec3 v = eye + worldDir*d;
+  vec3 worldDir = viewMatrix(eye, center, up) * ray;
+  vec3 rot;
+  float d = rayMarch(eye, worldDir, rot);
+  
+  vec3 i3;
 
   if(d < MaxDist){
+    vec3 v = eye + worldDir*d;
     vec3 n = estimateNormal(v);
 
-    float intensity = getColor(n);
+    vec3 nt = (vec4(n,1) * r2dX(rot.x)).xyz;
 
-    float lights = lighting(v, n, lightPos);
-    // i = lights/1.3 * col.x;
-    // i = col.x;
-    i = intensity;
+    float lights = lighting(v, n, lightPos, eye);
+    i3 = vec3(getColor(nt,v)) * lights;
+
+    i3 = pow(i3, 1./vec3(2.2));
   }
 
-  gl_FragColor = vec4(vec3(i), 1);
+  gl_FragColor = vec4(vec3(i3), 1);
 }
