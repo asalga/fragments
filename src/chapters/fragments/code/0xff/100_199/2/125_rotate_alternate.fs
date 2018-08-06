032aefee1ad4c6b502b199370684fcb1d6a12aac
Andor Saga <andor.saga@gmail.com>
2018-08-06 14:04:30 -0400

    125

--- src/chapters/fragments/code/0xff/100_199/2/125_rotate_alternate.fs
+++ src/chapters/fragments/code/0xff/100_199/2/125_rotate_alternate.fs
@@ -1,3 +1,4 @@
+// 125 - "Rotate, Alternate"
 precision mediump float;
 
 uniform vec2 u_res;
@@ -7,6 +8,17 @@ const float light = 0.8;
 const float dark = 0.3;
 const vec2 tileSz = vec2(10.);
 const float sz = 40.;
+
+mat2 r2d(float a){
+	return mat2(cos(a),sin(a),-cos(a), sin(a));
+}
+
+float sdRect(vec2 p, vec2 sz){
+  vec2 d = abs(p)-sz;
+  float _out = length(max(d, 0.));
+  float _in = min(max(d.y,d.x),0.);
+  return _in + _out;
+}
 
 void main(){
 	vec2 p = gl_FragCoord.xy;
@@ -20,6 +32,20 @@ void main(){
   float iy = step(sz/2., my);
   float mx = mod(p.x+ (iy*sz/2.), sz);
   float ix = step(sz/2., mx);
+
+
+  // 2) 
+
+
+
+
+  // 3) Rotate Tiles
+
+
+  // 4) 
+
+
+  // 5)
 
   float i = ix;
 
--- src/chapters/fragments/code/0xff/templates/_sdfs.fs
+++ src/chapters/fragments/code/0xff/templates/_sdfs.fs
@@ -6,7 +6,7 @@ float sdCircle(vec2 p, float r){
 
 float sdRect(vec2 p, vec2 sz){
   vec2 d = abs(p)-sz;
-  float _out = length(max( d , 0. ));
+  float _out = length(max(d, 0.));
   float _in = min(max(d.y,d.x),0.);
   return _in + _out;
 }
