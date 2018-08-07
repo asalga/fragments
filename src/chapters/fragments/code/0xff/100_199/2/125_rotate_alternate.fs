2701d6fbafa9e475a1bbf9ddfb0819073a86a282
Andor Saga <andor.saga@gmail.com>
2018-08-07 10:54:26 -0400

    125

--- src/chapters/fragments/code/0xff/100_199/2/125_rotate_alternate.fs
+++ src/chapters/fragments/code/0xff/100_199/2/125_rotate_alternate.fs
@@ -4,7 +4,7 @@ precision mediump float;
 uniform vec2 u_res;
 uniform float u_time;
 const vec2 tileSz = vec2(40.);
-const float sz = 60.;
+const float sz = 40.;
 const float PI = 3.141592658;
 
 mat2 r2d(float a){
@@ -20,19 +20,15 @@ float sdRect(vec2 p, vec2 sz){
 
 void main(){
 	vec2 p = gl_FragCoord.xy;
-	float t = u_time * 0.5 * PI + 7.;
-	p += t*40.;
+	float t = u_time * 0.5 * PI;
+	p += t * 40.;
 	float i = 0.;
-	
   float alt = step(PI/2., mod(t, PI));
 
-	if(alt < 1.){
-  	p.x += sz;
- 	}
+  p.x += (alt < 1.) ? sz: 0.;
 
-  vec2 rp = ((mod(p, sz)/sz) * 2.) -1.;
+  vec2 repP = ((mod(p, sz)/sz) * 2.) -1.;
   
-
   // [0,0] [1,0] [2,0] ...
   // [0,1] [1,1] [2,2] ...
   vec2 cell = floor(p/sz);
@@ -42,16 +38,16 @@ void main(){
 	// if even, draw 1 rect
 	bool isEven = mod(cell.x + cell.y, 2.) < 1.;
 	if(isEven){
-		rp *= r;
-  	i = step(sdRect(rp, vec2(1.)), 0.);	
+		repP *= r;
+  	i = step(sdRect(repP, vec2(1.)), 0.);	
   }
   // draw 4 surrounding rects
   else {
   	vec2 one = vec2(1.);
-  	i += step(sdRect( (rp - vec2( 0,  2.)) * r, one), 0.);	// top
-  	i += step(sdRect( (rp - vec2( 0, -2.)) * r, one), 0.);	// bottom
-  	i += step(sdRect( (rp - vec2( 2,  0.)) * r, one), 0.);
-  	i += step(sdRect( (rp - vec2(-2,  0.)) * r, one), 0.);
+  	i += step(sdRect( (repP - vec2( 0,  2.)) * r, one), 0.);	// top
+  	i += step(sdRect( (repP - vec2( 0, -2.)) * r, one), 0.);	// bottom
+  	i += step(sdRect( (repP - vec2( 2,  0.)) * r, one), 0.);
+  	i += step(sdRect( (repP - vec2(-2,  0.)) * r, one), 0.);
 
   	if(alt < 1.){
   		// i = 1.-i;
