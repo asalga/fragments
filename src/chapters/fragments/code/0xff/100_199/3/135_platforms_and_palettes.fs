126db75b332124791ae99e647bbafb34f9299812
Andor Saga <andor.saga@gmail.com>
2018-08-17 07:43:37 -0400

    135

--- src/chapters/fragments/code/0xff/100_199/3/135_platforms_and_palettes.fs
+++ src/chapters/fragments/code/0xff/100_199/3/135_platforms_and_palettes.fs
@@ -1,11 +1,11 @@
-// 135 - "Impossibilities"
+// 135 - "Platforms_and_Palettes"
 
 precision mediump float;
 
 uniform vec2 u_res;
 uniform float u_time;
 
-const float timeScale = 1.5;
+const float timeScale = 1.25;
 const float floorHeight = 0.1;
 const float PI = 3.141592658;
 const float TAU = PI*2.0;
@@ -119,7 +119,6 @@ float mushroom(in vec2 p){
   return i;
 }
 
-
 float ground(in vec2 p){
   return step(sdRect(p, vec2(1., floorHeight)),0.);
 }
@@ -128,21 +127,25 @@ mat2 rot2D(in float a){
   return mat2(cos(a), sin(a), -sin(a), cos(a));
 }
 
-float boxDude(in vec2 p){
-  float t = u_time*timeScale*PI * 1.0;
-  float boxSz = 0.06;
+float boxDude(in vec2 p, in float sc, in float warp){
+
+  // p.x -= (1.-warp)/1.;
+
+  float t = (u_time+0.) * timeScale*PI;
+  float boxSz = 0.06*sc;
   float buff = 0.01;
 
   float boxJumpPos = abs(sin(t)) * 0.15;
-  vec2 boxPos = vec2(0.5, boxSz + floorHeight + buff + boxJumpPos);
+  vec2 boxPos = vec2(0.5, boxSz + floorHeight + 0. + boxJumpPos);
   // vec2 boxPos = vec2(0.5);
 
-  vec2 finalTrans = rot2D(t) * (p-boxPos);
+  vec2 tr = rot2D(t) * (p-boxPos);
   // vec2 finalTrans = p-boxPos;
-  // p *= 0.032;
 
-  // return mushroom(finalTrans);
-  return step(sdRect(finalTrans, vec2(boxSz)), 0.);// * fract( length(p-boxPos)*50. + t/10. );
+
+  float fill = step( sdRect(tr, vec2(boxSz)) , 0.);
+
+  return fill;
 }
 
 float obstacle(in vec2 p){
@@ -159,38 +162,25 @@ float obstacle(in vec2 p){
   return sdS_Rect(p - pos, vec2(obsSz, 0.025));// * 0.5;
 }
 
-float bricks(vec2 p){
-  // p = (p/u_res);
-  float t = -u_time * 1.0;
+float bricks(vec2 p, float sc, float offset, float tsc){
+  float t = u_time* timeScale* 0.18 * tsc;
   float i;
-  vec3 col;
   float ROWS = u_res.y/16.;
-
   float NUM_BRICK_LINES = 4.;
   float noiseScale = 0.08;
 
-  p /= 4.;
+  p.y += offset;
+
+  p *= sc;
   p.x += t;
+  // p.x += 13.;
 
-  // float idx = floor(p.x*ROWS) * noiseScale;
   float n = 1.;
-  // n += smoothValueNoise(vec2(idx,0.)*2.)*0.5;
-  // n += smoothValueNoise(vec2(idx,0.)*4.)*0.25;
-  // n += smoothValueNoise(vec2(idx,0.)*8.)*0.125;
-  // n /= 1.5;
-  // n = clamp(n,0.,1.);
-
-  // Each column will get a different noise value
-  // based on the 'index'
-  float columnHeight = n;
-
-  // n = floor(columnHeight*ROWS)/ROWS;
-  // n = step(p.y,n);
 
   // add brick lines
   // "16 tiles rows" high * lines per tile
   // morter is at every 3/4 of a pixel
-  float horizLines = step(fract(p.y * ROWS * NUM_BRICK_LINES), (3./4.));
+  float horizLines = step(fract( p.y * ROWS * NUM_BRICK_LINES), (3./4.));
   n *= horizLines;
 
   // shift x every other row inside the tile
@@ -198,20 +188,17 @@ float bricks(vec2 p){
   float vertLines = step(fract( (p.x+shift/ROWS) * ROWS*2.), SEVEN_EIGHTS);
   n *= vertLines;
 
-  col = vec3(n);
-  // gl_FragColor = vec4(col,1);
-  return col.x;
+  return n;
 }
 
 void main(){
   vec2 p = gl_FragCoord.xy/u_res;
+  float t = u_time*0.1;
   float i;
-  i += bricks(p)*0.25;
-  i += ground(p) + boxDude(p) + obstacle(p);
-  // i = obstacle(p);
-  // i = boxDude(p);
 
-
-
+  i += sdS_Rect(p - vec2(0.6), vec2(1., 0.5)) * bricks(p, 0.25, 0., 0.5) * p.y*p.y;
+  i += obstacle(p);
+  i += ground(p) * bricks(p, .15, 0.300, .98) * 0.8;
+  i += boxDude(p, 1., 0.);
   gl_FragColor = vec4(vec3(i),1);
 }
