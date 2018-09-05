138a0441624966f8c101607ff939a80ef2665d24
Andor Saga <andor.saga@gmail.com>
2018-09-05 10:48:24 -0400

    155

--- src/chapters/fragments/code/0xff/100_199/5/156_connexion.fs
+++ src/chapters/fragments/code/0xff/100_199/5/156_connexion.fs
@@ -3,7 +3,7 @@ precision mediump float;
 uniform vec2 u_res;
 uniform float u_time;
 
-const int NUM = 14;
+const int NUM = 10;
 
 float valueNoise(float i, float scale){
   return fract(sin(i*scale));
@@ -28,16 +28,17 @@ void main(){
   vec2 pos[NUM];
   vec2 vel[NUM];
   vec2 fpos[NUM];
+  float connections[NUM];
 
   for(int it=0; it < NUM; ++it){
     float fit = float(it);
 
-    vec2 v = vec2(valueNoise((fit+1.) * (fit+1.), fit*198323.712983),
-                  valueNoise((fit+1.) * (fit+1.), fit*128392.234923));
+    vec2 v = vec2(valueNoise((fit+1.) * (fit+1.), fit*89323.712983),
+                  valueNoise((fit+1.) * (fit+1.), fit*12392.234923));
     vel[it] = v*2.-1.;
 
-    vec2 p = vec2(valueNoise(fit*fit, fit*198323.523),
-                  valueNoise(fit*fit, fit*9692033.623));
+    vec2 p = vec2(valueNoise(fit*fit, fit*19323.523),
+                  valueNoise(fit*fit, fit*46239.627));
     pos[it] = p*2.-1.;
 
     vec2 _pos = pos[it] + vel[it] * t;
@@ -51,22 +52,50 @@ void main(){
 
     fpos[it] = finalPos;//(finalPos-.5)*1.5;
 
-    // inten += step(sdCircle(pp + finalPos, 0.05),0.);
-    inten += .0005/(sdCircle(pp + finalPos, 0.001)/20.);
+
   }
 
    for(int i = 0; i < NUM; ++i){
     for(int j = 0; j < NUM; ++j){
-       if(i >= j) continue;
+       //if(i >= j) continue;
       vec2 p1 =  fpos[i];
       vec2 p2 =  fpos[j];
 
-      if( length(p1 - p2) < 0.5 ) {
-      // inten +=  step(sdLine(p, -_pos1, -_pos2, .005), 0.);
-         inten += abs(2./(sdLine(pp, -p1, -p2, 0.00001) * 2000.));
+      float _dist = (sin(u_time*5.)+1.)/4.;
+
+      vec2 v = p1 - p2;
+      float square = dot(v,v);
+      if( square < _dist ) {
+
+        connections[i]++;
+        // connections[j]++;
+
+        square = _dist - square;
+        square = square*square * 40.;
+        // square = pow(1.1, square);
+        //1./pow(square,2.);
+
+        // square = max(.5, square);
+
+        // inten += step(sdLine(p, -p1, -p2, .005), 0.);
+
+         inten +=  step(sdLine(pp, -p1, -p2,  0.001 * square), 0.);
+
+         inten += abs(2./(sdLine(pp, -p1, -p2,  0.001 * square ) * 3000.));
       }
     }
   }
+
+   for(int it=0; it < NUM; ++it){
+      vec2 finalPos = fpos[it];
+      float sz = connections[it]*3.;
+
+
+      inten += (.002)/(sdCircle(pp + finalPos, 0.0014 * sz));
+      inten = clamp(inten, 0., 1.);
+
+      inten += step(sdCircle(pp + finalPos, 0.0014 * sz ), 0.);
+   }
 
   gl_FragColor = vec4(vec3(inten),1.);
 }
--- src/chapters/fragments/code/0xff/100_199/5/158_.fs
+++ src/chapters/fragments/code/0xff/100_199/5/158_.fs
@@ -1,9 +1,7 @@
-// 158
-// TODO:
-// every 2 seconds, change y noise
-
+// 158 "Building"
 precision mediump float;
 
+#define SEVEN_EIGHTS (7./8.)
 uniform vec2 u_res;
 uniform float u_time;
 
@@ -12,6 +10,26 @@ float sdRect(in vec2 p, in vec2 sz){
   float _out = length(max(d, 0.));
   float _in = min(max(d.y,d.x),0.);
   return _in + _out;
+}
+
+float bricks(vec2 p, float sc){
+  float i;
+  float ROWS = u_res.y/16.;
+  float NUM_BRICK_LINES = 4.;
+  float n = 1.;
+
+  // add brick lines
+  // "16 tiles rows" high * lines per tile
+  // morter is at every 3/4 of a pixel
+  float horizLines = step(fract( p.y * ROWS * NUM_BRICK_LINES), (3./4.));
+  n *= horizLines;
+
+  // shift x every other row inside the tile
+  float shift = SEVEN_EIGHTS *2.* (step(fract(p.y*ROWS*2.), SEVEN_EIGHTS/2.));
+  float vertLines = step(fract( (p.x+shift/ROWS) * ROWS*2.), SEVEN_EIGHTS);
+  n *= vertLines;
+
+  return n;
 }
 
 float valueNoise(vec2 p){
@@ -24,16 +42,19 @@ float valueNoise(vec2 p){
 
 void main(){
   vec2 p = gl_FragCoord.xy/u_res*2.-1.;
+  vec2 op = gl_FragCoord.xy/u_res;
   float i;
 
-  float t = u_time * 0.5;
-  float ft = floor(u_time*0.5);
-  t = fract(t);
+  float t = u_time * 0.25;
+  float gt = t;
 
-  vec2 c = vec2(0.2);
-  vec2 rp = mod(p, c)-(c*.5);
+  float ft = floor(gt * 0.5);
+  t = fract(gt);
 
-  vec2 normed = gl_FragCoord.xy/u_res;
+  vec2 sz = vec2(0.2);
+  vec2 rp = mod(p, sz)-(sz*.5);
+
+  vec2 normed = op;
   vec2 cell = floor(normed*10.)/10.;
   cell.x += ft;
 
@@ -43,17 +64,21 @@ void main(){
   // noise value = 0..1 for all values
 
   // vary time for each column?
-  float variation = 1.;
+  float variation = 2.;
   t *= 1. + valueNoise(vec2(cell.x, cell.x)) * variation;
 
-  if(t > cell.y + n){
-    float fillTime = t - cell.y - n/2.;
-    vec2 sz = vec2(0.2) * fillTime * 12.;
+  i = 1.-bricks(op/4., 1.);
 
-    i += step(sdRect(rp, sz), 0.);
+  if(t > cell.y){
+    float fillTime = t - cell.y;
+    vec2 fillSz = sz * fillTime * 20.;
+
+    i -= step(sdRect(rp, fillSz), 0.);    // cut a hole
+    i = clamp(i, 0., 1.);
+    i += step(sdRect(rp, fillSz), 0.) * (bricks(op/4., 1.));
   }
 
-  if( mod(u_time, 4.) < 2.){
+  if( mod(gt, 2.) < 1.){// flip colors
     i = 1.-i;
   }
 
