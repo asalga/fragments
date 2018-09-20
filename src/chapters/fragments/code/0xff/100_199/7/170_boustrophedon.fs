2fc4793239926cf9b384049d757efd1a3ffe699a
Andor Saga <andor.saga@gmail.com>
2018-09-19 22:28:21 -0400

    .

--- src/chapters/fragments/code/0xff/100_199/7/170_boustrophedon.fs
+++ src/chapters/fragments/code/0xff/100_199/7/170_boustrophedon.fs
@@ -3,6 +3,9 @@ precision mediump float;
 uniform vec2 u_res;
 uniform float u_time;
 
+const float Rows = 10.;
+const float VertSpacing = .2;
+
 float sdRect(in vec2 p, in vec2 sz){
   vec2 d = abs(p)-sz;
   float _out = length(max(d, 0.));
@@ -10,27 +13,54 @@ float sdRect(in vec2 p, in vec2 sz){
   return _in + _out;
 }
 
-/*
-  1) show grid
-*/
-
 void main(){
   vec2 p = gl_FragCoord.xy/u_res;
   float i;
-  float t = u_time;
+  float t = u_time + 5.;
 
-  // i = step(sdRect(p, vec2(0.25)), 0.);
+  t = mod(u_time, Rows) * .4;// * 0.;
+  // p.y += u_time/15.;
 
-  // p.y -= .5;
-  // p.y += t/10.;
+  float rowID = floor(p.y * Rows);
 
-  float y = floor(p.y*10.);
+  float y = floor(p.y * Rows);
 
-  float md = mod(t,1.);
-  i = step(p.x, md);
+  float _x;
+  float _y = step(y, t-1.);
 
-  // i = step(t, y);
 
-  vec2 grid = mod(p, .1)*10.;
-  gl_FragColor = vec4(vec3(i,grid),1.);
+  // make the snake thinner
+  float localY = fract(p.y*10.);
+  float horizStrip = step(localY, 1.-VertSpacing) * step(VertSpacing, localY);
+
+  // if floored time is on the current x/horizontal line that we are on
+  float rowSelected = floor(t);
+  if( rowID == rowSelected){
+    float ft = fract(t);
+    float x = p.x;
+
+    // if even, we go backwards, so flip x
+    if(mod(rowID,2.) == 0.){
+      x = 1.-x;
+    }
+
+    // 0 if ft < edge
+    // 1 if ft > edge
+    _x = step(x, ft) * horizStrip;
+  }
+
+  vec2 p2 = (gl_FragCoord.xy/u_res)*2.-1.;
+  float straightLineSpace = step(sdRect(p2, vec2(0.8, 1.)), 0.);
+
+
+
+
+  i = _y * horizStrip * straightLineSpace;
+  i += _x * straightLineSpace;
+
+
+
+  vec2 grid = mod(p, .1) * Rows;
+  // gl_FragColor = vec4(vec3(i,grid),1.);
+  gl_FragColor = vec4(vec3(i),1.);
 }
